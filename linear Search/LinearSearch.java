aa6c07654930c825b952f564e81aad72863ad191
Dinesha <dineshapriyadarshani96@gmail.com>
2018-08-29 22:47:01 +0530

    hhj

---
+++ linear Search/LinearSearch.java
@@ -0,0 +1,16 @@
+class LinearSearch
+{
+    
+    static int search(int arr[], int n, int x)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            
+            if (arr[i] == x)
+                return i;
+        }
+ 
+        
+        return -1;
+    }
+} 
