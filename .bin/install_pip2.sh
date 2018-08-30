ea7e2a1464f9a46db11a618bdf7df69d722fe031
Danushka Herath <Danushkaherath96@gmail.com>
2018-08-30 09:48:08 +0530

    modify pip install travis

--- .bin/install_pip2.sh
+++ .bin/install_pip2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo -H pip2 install --upgrade pip  --ignore-installed urllib3
-sudo -H pip2 install -Ur pip2-requirements.txt sudo -H pip2 install --upgrade pip  --ignore-installed urllib3
+sudo -H pip2 install -Ur pip2-requirements.txt --ignore-installed urllib3
