dbf72d99c074f4688a61468cecd02af8c4fb7401
Danushka Herath <Danushkaherath96@gmail.com>
2018-09-01 18:25:33 +0530

    changed the test file

--- .bin/test_running.sh
+++ .bin/test_running.sh
@@ -11,16 +11,16 @@ for i in $(ls -1 **/*.c); do
     echo ""
 done
 
-echo ""
-echo "Testing C++ files..."
-for i in $(ls -1 **/*.cpp); do
-    echo "    Compiling $i - g++ $i -lm -pthread -std=c++11"
-    g++ $i -lm -pthread -std=c++11
-    echo "    Running $i - ./a.out > /dev/null"
-    ./a.out > /dev/null
-    rm -f a.out
-    echo ""
-done
+# echo ""
+# echo "Testing C++ files..."
+# for i in $(ls -1 **/*.cpp); do
+#     echo "    Compiling $i - g++ $i -lm -pthread -std=c++11"
+#     g++ $i -lm -pthread -std=c++11
+#     echo "    Running $i - ./a.out > /dev/null"
+#     ./a.out > /dev/null
+#     rm -f a.out
+#     echo ""
+# done
 
 # echo ""
 # echo "Testing Java files..."
@@ -37,28 +37,28 @@ done
 
 echo "Testing Python files..."
 for i in $(ls -1 **/*.py); do
-    echo "    Running $i - python2 $i > /dev/null"
-    python2 $i > /dev/null
+    # echo "    Running $i - python2 $i > /dev/null"
+    # python2 $i > /dev/null
     echo "    Running $i - python3 $i > /dev/null"
     python3 $i > /dev/null
     echo ""
 done
 
-echo ""
-echo "Running Go files..."
-for i in $(ls -1 **/*.go); do
-    echo "    Running $i - go run $i > /dev/null"
-    go run $i > /dev/null
-    echo ""
-done
+# echo ""
+# echo "Running Go files..."
+# for i in $(ls -1 **/*.go); do
+#     echo "    Running $i - go run $i > /dev/null"
+#     go run $i > /dev/null
+#     echo ""
+# done
 
-echo ""
-echo "Running C# files..."
-for i in $(ls -1 **/*.cs); do
-    echo "    Compiling $i - mcs $i"
-    mcs $i
-    excname="${i%.*}".exe
-    echo "    Running $i - mono $excname > /dev/null"
-    mono $excname > /dev/null
-    echo ""
-done
+# echo ""
+# echo "Running C# files..."
+# for i in $(ls -1 **/*.cs); do
+#     echo "    Compiling $i - mcs $i"
+#     mcs $i
+#     excname="${i%.*}".exe
+#     echo "    Running $i - mono $excname > /dev/null"
+#     mono $excname > /dev/null
+#     echo ""
+# done
