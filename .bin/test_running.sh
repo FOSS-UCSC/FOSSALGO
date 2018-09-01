4c09fd8191b95bcb699cd9480903bfea491ab7b5
Danushka Herath <Danushkaherath96@gmail.com>
2018-09-01 18:28:38 +0530

    changed the test file

--- .bin/test_running.sh
+++ .bin/test_running.sh
@@ -35,14 +35,14 @@ done
 # done
 # rm -f *.class
 
-echo "Testing Python files..."
-for i in $(ls -1 **/*.py); do
-    # echo "    Running $i - python2 $i > /dev/null"
-    # python2 $i > /dev/null
-    echo "    Running $i - python3 $i > /dev/null"
-    python3 $i > /dev/null
-    echo ""
-done
+# echo "Testing Python files..."
+# for i in $(ls -1 **/*.py); do
+#     # echo "    Running $i - python2 $i > /dev/null"
+#     # python2 $i > /dev/null
+#     echo "    Running $i - python3 $i > /dev/null"
+#     python3 $i > /dev/null
+#     echo ""
+# done
 
 # echo ""
 # echo "Running Go files..."
@@ -61,4 +61,4 @@ done
 #     echo "    Running $i - mono $excname > /dev/null"
 #     mono $excname > /dev/null
 #     echo ""
-# done
+done
