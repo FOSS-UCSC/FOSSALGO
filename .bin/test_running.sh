fe2057c8198a2bbcc8c4979ffeff694c42c07983
Danushka Herath <Danushkaherath96@gmail.com>
2018-09-07 22:15:32 +0530

    rebase folder structure

--- .bin/test_running.sh
+++ .bin/test_running.sh
@@ -11,29 +11,29 @@ for i in $(ls -1 **/*.c); do
     echo ""
 done
 
-# echo ""
-# echo "Testing C++ files..."
-# for i in $(ls -1 **/*.cpp); do
-#     echo "    Compiling $i - g++ $i -lm -pthread -std=c++11"
-#     g++ $i -lm -pthread -std=c++11
-#     echo "    Running $i - ./a.out > /dev/null"
-#     ./a.out > /dev/null
-#     rm -f a.out
-#     echo ""
-# done
+echo ""
+echo "Testing C++ files..."
+for i in $(ls -1 **/*.cpp); do
+    echo "    Compiling $i - g++ $i -lm -pthread -std=c++11"
+    g++ $i -lm -pthread -std=c++11
+    echo "    Running $i - ./a.out > /dev/null"
+    ./a.out > /dev/null
+    rm -f a.out
+    echo ""
+done
 
-# echo ""
-# echo "Testing Java files..."
-# for i in $(ls -1 **/*.java); do
-#     echo "    Compiling $i - javac -Werror -Xlint:all $i -d ."
-#     javac -Werror -Xlint:all $i -d .
-#     filename="${i##*/}"
-#     classname="${filename%.*}"
-#     echo "    Running $i - java $classname > /dev/null"
-#     java $classname > /dev/null
-#     echo ""
-# done
-# rm -f *.class
+echo ""
+echo "Testing Java files..."
+for i in $(ls -1 **/*.java); do
+    echo "    Compiling $i - javac -Werror -Xlint:all $i -d ."
+    javac -Werror -Xlint:all $i -d .
+    filename="${i##*/}"
+    classname="${filename%.*}"
+    echo "    Running $i - java $classname > /dev/null"
+    java $classname > /dev/null
+    echo ""
+done
+rm -f *.class
 
 echo "Testing Python files..."
 for i in $(ls -1 */*.py); do
@@ -44,21 +44,21 @@ for i in $(ls -1 */*.py); do
     echo ""
 done
 
-# echo ""
-# echo "Running Go files..."
-# for i in $(ls -1 **/*.go); do
-#     echo "    Running $i - go run $i > /dev/null"
-#     go run $i > /dev/null
-#     echo ""
-# done
+echo ""
+echo "Running Go files..."
+for i in $(ls -1 **/*.go); do
+    echo "    Running $i - go run $i > /dev/null"
+    go run $i > /dev/null
+    echo ""
+done
 
-# echo ""
-# echo "Running C# files..."
-# for i in $(ls -1 **/*.cs); do
-#     echo "    Compiling $i - mcs $i"
-#     mcs $i
-#     excname="${i%.*}".exe
-#     echo "    Running $i - mono $excname > /dev/null"
-#     mono $excname > /dev/null
-#     echo ""
-#done
+echo ""
+echo "Running C# files..."
+for i in $(ls -1 **/*.cs); do
+    echo "    Compiling $i - mcs $i"
+    mcs $i
+    excname="${i%.*}".exe
+    echo "    Running $i - mono $excname > /dev/null"
+    mono $excname > /dev/null
+    echo ""
+done
