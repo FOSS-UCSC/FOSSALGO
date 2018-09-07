68761e5d024492a9c6730f662f6432ab3d6dc550
Danushka Herath <Danushkaherath96@gmail.com>
2018-09-07 21:23:16 +0530

    fix issue

--- .bin/install_pip3.sh
+++ .bin/install_pip3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo -H pip3 install --upgrade pip
-sudo -H pip3 install -Ur pip3-requirements.txt
+sudo -H pip3 install --upgrade pip --ignore-installed six
+sudo -H pip3 install -Ur pip3-requirements.txt --ignore-installed six
