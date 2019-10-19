40beea0f47b1158779c51a938ee9abc0ef521d66
RaninduJ <55150369+RaninduJ@users.noreply.github.com>
2019-10-19 14:21:05 +0530

    Update Queue.c

--- queue/Queue.c
+++ queue/Queue.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #define max 10
+int array[max], val, i;
+int front = -1;
+int rear = -1;
 
+void enQueue(val){
+  if(rear == max-1)
+    printf("Queue is full");
+  else{
+    if(front == -1){
+      front = 0;
+    }
+    rear = rear+1;
+    array[rear] = val;
+  }
+}
