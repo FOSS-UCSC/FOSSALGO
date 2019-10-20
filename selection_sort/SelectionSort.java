ab18b51a22d82a77144ef9ba8b88738332147036
Vikum-Kule <56560837+Vikum-Kule@users.noreply.github.com>
2019-10-21 01:10:19 +0530

    fixed bugs and developed code

--- selection_sort/SelectionSort.java
+++ selection_sort/SelectionSort.java
@@ -11,15 +11,20 @@ class SelectionSort
         for (int i = 0; i < n-1; i++)
         {
             // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (int j = i+1; j < n; j++)
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
+            //int min_idx = i;
+            int min=arr[i];
+            int index=i;
+            for (int j = i+1; j < n; j++){
+                if (arr[j] < min){
+                    min=arr[j];
+                    index=j;
+                }
+            }
  
             // Swap the found minimum element with the first
             // element
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
+            int temp = arr[index];
+            arr[index] = arr[i];
             arr[i] = temp;
         }
     }
