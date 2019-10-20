1d6894f8ef430402ca138932920264d114f51f78
AnukiAlwis <anu.alwis1998@gmail.com>
2019-10-20 18:12:23 +0530

    allows users to sort any length of an array.

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 void main()
 {
-    int ar[5],i;
-    for (i=0;i<5;i++)
+	int n=1;
+	printf("Enter the numeber of items in the list:");
+        scanf("%d", &n);
+    int ar[n],i;
+    for (i=0;i<n;i++)
     {
         printf("Input number %d :",i+1);
         scanf("%d", &ar[i]);
     }
     printf("\nOriginal List : ");
-    for (i=0;i<5;i++)
+    for (i=0;i<n;i++)
         printf("%d ",ar[i]);
 
     int tmp,j,k;
-    for (j=0;j<5;j++)
+    for (j=0;j<n;j++)
     {
-        for (k=0;k<4;k++)
+        for (k=0;k<n-1;k++)
         {
             if (ar[k]>ar[k+1])
             {
@@ -26,7 +29,7 @@ void main()
     }
     printf("\n\nSorted array :");
 
-    for (i=0;i<5;i++)
+    for (i=0;i<n;i++)
         printf("%d ", ar[i]);
 
 }
