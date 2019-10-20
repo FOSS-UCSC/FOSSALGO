1af79ae0fe7a1f60e3f172384054c4c866a4fa70
VinuUD <vinudayaratne@gmail.com>
2019-10-20 21:18:10 +0530

    Split the binary_search.py file into seperate functions so as to easily understand the algorithm

--- binary_search/Binary_search.py
+++ binary_search/Binary_search.py
@@ -1,34 +1,49 @@
-array = []
-for i in range(11):
-    a = int(input("enter num :"))
-    b = array.append(a)
-print("Array --------> ", array)
+def sort(array):
+    for i in range(len(array) - 1, 0, -1):
+        for j in range(i):
+            if (array[j] > array[j + 1]):
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+                
+    print("\nSorted Array --------> ", array)
 
-for i in range(len(array) - 1, 0, -1):
-    for j in range(i):
-        if (array[j] > array[j + 1]):
-            temp = array[j]
-            array[j] = array[j + 1]
-            array[j + 1] = temp
+def search(c):  
+    aLen = len(array)
+    t = aLen // 2
 
-print("\nSorted Array --------> ", array)
+    while((t != -1) and (t < aLen)):
+        if(c > array[t]):
+            t = (t + aLen + 1) // 2
+        elif(c < array[t]):
+            t = t // 2
+        elif(c == array[t]):
+            break
+        else:
+            t = aLen + 1
 
-c = int(input("\nEnter search num :"))
-
-aLen = len(array)
-t = aLen // 2
-
-while((t != -1) and (t < aLen)):
-    if(c > array[t]):
-        t = (t + aLen + 1) // 2
-    elif(c < array[t]):
-        t = t // 2
-    elif(c == array[t]):
-        break
+    if(t < aLen):
+        print("search is finished. Index is :", t)
     else:
-        t = aLen + 1
+        print("search is finished.Value Not found.")
 
-if(t < aLen):
-    print("search is finished. Index is :", t)
-else:
-    print("search is finished.Value Not found.")
+if __name__ == '__main__':
+
+    array = []
+    for i in range(11):
+        a = int(input("enter num :"))
+        b = array.append(a)
+    print("Array --------> ", array)
+
+    sort(array)
+    
+    c = int(input("\nEnter search num :"))
+    
+    search(c)
+    
+def main():
+    exit
+
+
+
+
