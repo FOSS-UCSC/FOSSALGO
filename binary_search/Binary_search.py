cb3f799e0b2380849bcca63da40947a91ae45605
AnukiAlwis <anu.alwis1998@gmail.com>
2019-10-20 17:45:02 +0530

    user can define the array length :

--- binary_search/Binary_search.py
+++ binary_search/Binary_search.py
@@ -1,6 +1,10 @@
 array=[]
-for i in range(11):
-    a = int(input("enter num :"))
+Num=1
+
+Num= int (input("Enter the length of your array : "))
+
+for i in range(Num):
+    a = int(input("enter array items :"))
     b = array.append(a)
 print("Array --------> " ,array)
 
@@ -15,22 +19,22 @@ print("\nSorted Array --------> " ,array)
 
 c = int (input("\nEnter search num :"))
 new_array=[]
-if(c > array[5]):
-    for k in range(6,11):
+if(c > array[len(array)/2]):
+    for k in range((len(array)/2+1),len(array)+1):
         d = new_array.append(array[k])
     #print(new_array)
-    for l in range(11):
+    for l in range(Num):
         if (array[l] == c):
             print("search is finished. Index is :", l)
 
-elif(c == array[5]):
-    print("search is finished. Index is :", 5)
+elif(c == array[len(array)/2]):
+    print("search is finished. Index is :", len(array)/2)
 
 else:
-    for k in range(5):
+    for k in range(len(array)/2):
         e = new_array.append(array[k])
     print(new_array)
-    for l in range(11):
+    for l in range(len(array)+1):
         if (array[l] == c):
             print("search is finished. Index is :", l)
         
