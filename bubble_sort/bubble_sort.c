5ec1664a885658a74cd26c4b5f43021ece104e81
Thashwini Wattuhewa <thashwiniwattuhewa@gmail.com>
2019-10-21 16:45:20 +0530

    Changed Algorithm to sort n number of elements

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -4,21 +4,24 @@ void main()
 	//int num;
 	//printf("How many numbers are in the list : ");
 	//scanf("%d", &num);
-	
-    int ar[5],i;
-    for (i=0;i<5;i++)
+	int n;
+    
+    printf("Enter a value for n:");
+    scanf("%d",&n);
+    int ar[n],i;
+    for (i=0;i<n;i++)
     {
         printf("Input number %d : ",i+1);
         scanf("%d", &ar[i]);
     }
     printf("\nOriginal List : ");
-    for (i=0;i<5;i++)
+    for (i=0;i<n;i++)
         printf("%d ",ar[i]);
 
     int tmp,j,k;
-    for (j=0;j<5;j++)
+    for (j=0;j<n-1;j++)
     {
-        for (k=0;k<4;++k)
+        for (k=0;k<n-j-1;++k)
         {
             if (ar[k]>ar[k+1])
             {
@@ -30,7 +33,7 @@ void main()
     }
     printf("\n\nSorted array : ");
 
-    for (i=0;i<5;i++)
+    for (i=0;i<n;i++)
         printf("%d ", ar[i]);
 
 }
