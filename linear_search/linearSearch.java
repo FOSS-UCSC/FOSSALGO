bc5a9cd0f02440d69eef812bfe20646ee1e03473
Supun Nanayakkara <supunnanayakkara11@gmail.com>
2019-10-22 00:34:24 +0530

    Update linearSearch.java

--- linear_search/linearSearch.java
+++ linear_search/linearSearch.java
@@ -1,16 +1,29 @@
 class linearSearch
-{
-    
+{    
     static int search(int arr[], int n, int x)
     {
         for (int i = 0; i < n; i++)
         {
-            
             if (arr[i] == x)
                 return i;
-        }
- 
-        
+        } 
         return -1;
     }
+	
+	public static void main(String args[]) 
+	{ 
+		int arr[] = {21,34,12,3,10,45,19};  
+		int x = 10; 
+		int n = arr.length;
+		int result = search(arr,n,x); 
+		if(result == -1)
+		{
+			System.out.print("Element is not present in array");
+		} 
+		else
+		{
+			System.out.print("Element is present at index " + result);
+		}
+	}
 } 
+ 
