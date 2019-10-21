ceb1a00ea764e481a17860b96dc1197dbc315ba1
ChamikaD97 <56592121+ChamikaD97@users.noreply.github.com>
2019-10-21 20:26:35 +0530

    Team Code Builders-Updated to any number of inputs
    Update the program to any number of inputs and, display both ascending and descending order.

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 void main()
-{	
-	//int num;
-	//printf("How many numbers are in the list : ");
-	//scanf("%d", &num);
-	
-    int ar[5],i;
-    for (i=0;i<5;i++)
+{
+	int num;
+	printf("How many numbers are in the list : ");
+	scanf("%d", &num);
+
+    int ar[num],i;
+    for (i=0;i<num;i++)
     {
         printf("Input number %d : ",i+1);
         scanf("%d", &ar[i]);
     }
     printf("\nOriginal List : ");
-    for (i=0;i<5;i++)
+    for (i=0;i<num;i++)
         printf("%d ",ar[i]);
 
     int tmp,j,k;
-    for (j=0;j<5;j++)
+    for (j=0;j<num;j++)
     {
-        for (k=0;k<4;++k)
+        for (k=0;k<(num-1);++k)
         {
             if (ar[k]>ar[k+1])
             {
@@ -28,9 +28,11 @@ void main()
             }
         }
     }
-    printf("\n\nSorted array : ");
-
-    for (i=0;i<5;i++)
+    printf("\n\nSorted array in Ascending order: ");
+    for (i=0;i<num;i++)
         printf("%d ", ar[i]);
 
+    printf("\n\nSorted array in Descending order: ");
+    for (i=(num-1);i>=0;i--)
+        printf("%d ", ar[i]);
 }
