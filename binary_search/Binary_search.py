01d6706b86278b129414697819605c0482275731
Thashwini Wattuhewa <thashwiniwattuhewa@gmail.com>
2019-10-21 15:13:39 +0530

    removed duplicates from sorted array

--- binary_search/Binary_search.py
+++ binary_search/Binary_search.py
@@ -11,7 +11,9 @@ for i in range(len(array) - 1, 0, -1):
             array[j] = array[j + 1]
             array[j + 1] = temp
 
+array =list(set(array))
 print("\nSorted Array --------> ", array)
+
 
 c = int(input("\nEnter search num :"))
 
