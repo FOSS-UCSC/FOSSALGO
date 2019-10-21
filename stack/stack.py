298f3941ad68fba5042d3f33de1255a5f0064d04
Uthpala Ruwanara <uruwanara@users.noreply.github.com>
2019-10-21 23:05:39 +0530

    Update stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -1,25 +1,23 @@
-# Implementing the Stack class
-
 class Stack:
-    def __init__(self):
-        self.items = []
+     def __init__(self):
+         self.container = [] 
+     def isEmpty(self):
+         return self.size() == 0  
+     def push(self, item):
+         self.container.append(item)  
+     def pop(self):
+         print( self.container.pop() )
+     def peek(self):
+         if self.isEmpty():
+             raise Exception("Stack empty!")
+         return self.container[-1] 
+     def size(self):
+         return len(self.container) 
+     def show(self):
+         return self.container
 
-    def isEmpty(self):
-        return self.items == []
 
-    def push(self, ele):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        return self.items[len(self.items) - 1]
-
-    def size(self):
-        return len(self.items)
-
-    def printStack(self):
-        print(self.items)
-
-# End of Stack class
+newStack=Stack()
+newStack.push(10)
+newStack.push(30)
+newStack.pop()
