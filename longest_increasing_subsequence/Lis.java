5620a2e9c69265f3de89858561683f2c538aa8d3
hiru997 <hirushachamod@gmail.com>
2019-10-22 23:56:42 +0530

    Fixed the code

--- longest_increasing_subsequence/Lis.java
+++ longest_increasing_subsequence/Lis.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-class Lis {
+public class Lis {
 	public static void main (String [] args) {
 		Scanner s = new Scanner(System.in);
 		Random r = new Random();
