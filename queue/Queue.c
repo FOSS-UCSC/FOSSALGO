cc67dbace4e42de51b791c82f5bdc51f72495116
Chamika Deshan Jayasingha <56592121+ChamikaD97@users.noreply.github.com>
2019-10-22 21:20:45 +0530

    Team Code Builders-Update some funtions

--- queue/Queue.c
+++ queue/Queue.c
@@ -1,52 +1,76 @@
-#include <stdio.h>
-#define max 10
-int array[max], val, i;
-int front = -1;
-int rear = -1;
-
-void enQueue(val){
-  if(rear == max-1)
-    printf("Queue is full");
-  else{
-    if(front == -1){
-      front = 0;
-    }
-    rear = rear+1;
-    array[rear] = val;
-  }
-}
-
-void deQueue(){
-  if(front == -1)
-    printf("Queue is empty!);
-  else{
-    return( array[front]);
-    front = front+1;
-    if(front > rear){
-      front = -1;
-      rear = -1;
-    }
-  }
-}
-
-void display(){
-  if(rear == -1)
-    printf("Queue is empty");
-   else{
-    for(i=front; i<= rear; i++){
-      printf("%d" array[i]);
-    }
-   }
-}
+#include<stdio.h>
+#define max 50
+int num,val;
+int front=-1,back=-1;
+int que[max];
+enQueue(int);
+deQueue();
+display();
+isEmpty();
+isFull();
 
 int main(){
-  for (i=0;i<10;i++){
-    enQueue(i+5);
-  }
-  deQueue();
-  deQueue();
-  display();	
-	enQueue(35);
-	display();
-	
+    int x;
+    do{
+    printf("\n\t1:Insert Number to Queue \n\t2:Delete From Queue \n\t3:Print \n\t4:Exit \n");
+    scanf("%d",&x);
+    switch(x){
+        case 1:
+            printf("Enter Number to Add to the Queue:");
+            scanf("%d",&num);
+            enQueue(num);
+            break;
+        case 2:
+            deQueue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            break;
+    }
+    }while(x!=4);
+}
+int enQueue(int data){
+    if(isFull()==1)
+        printf("Queue Is Full");
+    else
+        if(front==back==-1){
+            front=back=0;
+            que[back]=data;
+        }
+        else
+            back=back+1;
+            que[back]=data;
+}
+int deQueue(){
+    if(isEmpty()==1){
+        printf("Queue Is Empty");
+    }
+    else{
+        val=que[front];
+        que[front]=999;
+        printf("Number %d is Deleted",val);
+        front=front+1;
+        }
+}
+int isEmpty(){
+    if(back==front==-1 || front>back)
+        return 1;
+    else
+        return 0;
+}
+int isFull(){
+    if(back==max)
+        return 1;
+    else
+        return 0;
+}
+int display(){
+    int i,j=back;
+    printf("Values In The Queue:");
+
+    for(i=0;i<=j;i++){
+        printf("%d  ",que[i]);
+    }
 }
