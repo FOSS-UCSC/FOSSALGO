3beb8102dbbc8f7d53d3b5ed0d100a87abee89ff
Chathura Janaranjana Wanniarachchi <wachathuraj@gmail.com>
2019-10-22 21:52:37 +0530

    Create stack.c
    Bulid code segment to insert(push) data into stack.hear defind a small stack with 4 memory cells.implementation is based on array.

---
+++ stack/stack.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void push(int);
+
+int stack[4];
+int size=sizeof(stack)/sizeof(int);
+int top=-1;
+int main(){
+	
+	return 0;
+}
+
+//value insertion
+void push(int data){
+	if(top!=size-1){
+		top=top+1;
+		stack[top]=data;
+	}else
+		printf("%d with Stack overflow\n",data);
+}
