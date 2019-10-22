34e62d6cf22778f803e01c0087c15f52953f211f
ShubhangiGR97 <56677123+ShubhangiGR97@users.noreply.github.com>
2019-10-22 14:57:52 +0530

    Team Code Builders--Update the code to anyinputs

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 void main()
 {	
-	//int num;
-	//printf("How many numbers are in the list : ");
-	//scanf("%d", &num);
+	int num;
+	printf("How many numbers are in the list : ");
+	scanf("%d", &num);
 	
     int ar[5],i;
-    for (i=0;i<5;i++)
+    for (i=0;i<num;i++)
     {
         printf("Input number %d : ",i+1);
         scanf("%d", &ar[i]);
     }
     printf("\nOriginal List : ");
-    for (i=0;i<5;i++)
+    for (i=0;i<num;i++)
         printf("%d ",ar[i]);
 
     int tmp,j,k;
-    for (j=0;j<5;j++)
+    for (j=0;j<num;j++)
     {
         for (k=0;k<4;++k)
         {
@@ -30,7 +30,7 @@ void main()
     }
     printf("\n\nSorted array : ");
 
-    for (i=0;i<5;i++)
+    for (i=0;i<num;i++)
         printf("%d ", ar[i]);
 
 }
