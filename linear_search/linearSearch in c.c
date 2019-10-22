a9e4f3249245a2f7f8be0562e9ab1f193a1f2407
MichelleFdo <mufernando02@gmail.com>
2019-10-22 23:28:18 +0530

    linearSearch algo
    team X001

---
+++ linear_search/linearSearch in c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int linearSearch(int values[], int target, int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        if (values[i] == target) 
+        {       
+            return i; 
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+	for i in 
+}
