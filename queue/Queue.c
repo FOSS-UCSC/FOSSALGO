70bd90f9fe039e2cc0550b6db9fbf7018adcd2c4
ReshaniD97 <56593064+ReshaniD97@users.noreply.github.com>
2019-10-22 15:03:25 +0530

    Team Code Builders-Update queue

--- queue/Queue.c
+++ queue/Queue.c
@@ -1,52 +1,66 @@
-#include <stdio.h>
-#define max 10
-int array[max], val, i;
-int front = -1;
-int rear = -1;
 
-void enQueue(val){
-  if(rear == max-1)
-    printf("Queue is full");
-  else{
-    if(front == -1){
-      front = 0;
-    }
-    rear = rear+1;
-    array[rear] = val;
-  }
+#include<stdio.h>
+#define SIZE 5
+
+
+void enQueue(int);
+void deQueue();
+void display();
+int items[SIZE], front = -1, rear = -1;
+int main()
+{
+    //deQueue is not possible on empty queue
+    deQueue();
+    
+    //enQueue 5 elements
+    enQueue(1);
+    enQueue(2);
+    enQueue(3);
+    enQueue(4);
+    enQueue(5);
+    
+    //6th element can't be added to queue because queue is full
+    enQueue(6);
+    
+    display();
+    
+    //deQueue removes element entered first i.e. 1
+    deQueue();
+    
+    //Now we have just 4 elements
+    display();
+    
+    return 0;
+    
 }
-
+void enQueue(int value){
+    if(rear == SIZE-1)
+        printf("\nQueue is Full!!");
+    else {
+        if(front == -1)
+            front = 0;
+        rear++;
+        items[rear] = value;
+        printf("\nInserted -> %d", value);
+    }
+}
 void deQueue(){
-  if(front == -1)
-    printf("Queue is empty!);
-  else{
-    return( array[front]);
-    front = front+1;
-    if(front > rear){
-      front = -1;
-      rear = -1;
+    if(front == -1)
+        printf("\nQueue is Empty!!");
+    else{
+        printf("\nDeleted : %d", items[front]);
+        front++;
+        if(front > rear)
+            front = rear = -1;
     }
-  }
 }
-
 void display(){
-  if(rear == -1)
-    printf("Queue is empty");
-   else{
-    for(i=front; i<= rear; i++){
-      printf("%d" array[i]);
+    if(rear == -1)
+        printf("\nQueue is Empty!!!");
+    else{
+        int i;
+        printf("\nQueue elements are:\n");
+        for(i=front; i<=rear; i++)
+            printf("%d\t",items[i]);
     }
-   }
-}
-
-int main(){
-  for (i=0;i<10;i++){
-    enQueue(i+5);
-  }
-  deQueue();
-  deQueue();
-  display();	
-	enQueue(35);
-	display();
-	
 }
