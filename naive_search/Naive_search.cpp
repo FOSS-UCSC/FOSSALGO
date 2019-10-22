c0f25f7d948aaeb35b283e26dd36f15262a3c234
Jananigamage <56596699+Jananigamage@users.noreply.github.com>
2019-10-22 22:54:14 +0530

    Update Naive_search.cpp

--- naive_search/Naive_search.cpp
+++ naive_search/Naive_search.cpp
@@ -1,29 +1,32 @@
-//c programe for Naive String matching algorithem
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int match(char *txt,char *pat){
-    int n=strlen(txt);
-    int m=strlen(pat);
-    int i,j,k;
-    for(i=0;i<(n-m+1);i++){
-	//check patern match for i
-        for(j=0;j<=m-1;j++){
-            if(txt[i+j]!=pat[j])
-                break;
-        if(j==m-1){
-           printf("Pattern found at %d\n",i);
-        }
-        }
-    }
-}
+#include <stdio.h> 
+#include <string.h> 
+  
+void search(char* txt, char* pat) 
+{ 
+    int M = strlen(pat); 
+    int N = strlen(txt); 
+  
+    /* A loop to slide pat[] one by one */
+    int i;
+    for (i = 0; i <= N - M; i++) { 
+        int j; 
+  
+        /* For current index i, check for pattern match */
+        for (j = 0; j < M; j++) 
+            if (txt[i + j] != pat[j]) 
+                break; 
+  
+        if (j == M) // if pat[0...M-1] = txt[i, i+1, ...i+M-1] 
+            printf("Pattern found at index %d \n", i); 
+    } 
+} 
 //driver programe
+
 int main()
 {
    char txt[]="ABCECGBCERBCETY";
    char pat[]="BCE";
-   match(txt,pat);
+   search(txt,pat);
    return 0;
 
 }
