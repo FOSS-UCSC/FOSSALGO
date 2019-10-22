5714e23c221b025345f91316173aa464fdd186c0
Supun Nanayakkara <supunnanayakkara11@gmail.com>
2019-10-22 23:15:16 +0530

    Update SelectionSort.java

--- selection_sort/SelectionSort.java
+++ selection_sort/SelectionSort.java
@@ -1,56 +1,38 @@
-
-import java.util.*;
-
-class SelectionSort
+import java.util.Scanner;
+public class SelectionSort 
 {
-    void sort(int arr[])
-    {
-        int n = arr.length;
- 
-        // One by one move boundary of unsorted subarray
-        for (int i = 0; i < n-1; i++)
-        {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (int j = i+1; j < n; j++)
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
- 
-            // Swap the found minimum element with the first
-            // element
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
-        }
-    }
- 
-    // Prints the array
-    void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
-        System.out.println();
-    }
- 
-    // Driver code to test above
-    public static void main(String args[])
-    {
-        SelectionSort ob = new SelectionSort();
-	Scanner s = new Scanner(System.in);
-	System.out.println("Insert array elements seperating with spaces only");
-	
-	String temp = s.nextLine();
+   public static void main(String args[])
+   {
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of the array:");
+		int size = sc.nextInt();
+		int [] array = new int[size];
+		System.out.println("Enter the elements of the array:");
 
-	String ar[] = temp.split(" ");
-        int arr[] = new int[ar.length];
+		for(int i=0; i<size; i++ ) 
+		{
+			array[i] = sc.nextInt();
+		}
 
-	for(int i=0;i<ar.length;i++){
-		arr[i] = Integer.parseInt(ar[i]);
-	}
+		for (int i = 0 ;i< size-1; i++)
+		{
+			int min = i;
 
-        ob.sort(arr);
-        System.out.println("Sorted array");
-        ob.printArray(arr);
-    }
+			for (int j = i+1; j<size; j++)
+			{
+				if (array[j] < array[min])
+				{
+					min = j;
+				}
+			}
+			int temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+		}
+		System.out.println("Sorted Array");
+		for (int i = 0 ;i< size; i++)
+		{
+			System.out.print(" "+array[i]);
+		}
+	}  
 }
