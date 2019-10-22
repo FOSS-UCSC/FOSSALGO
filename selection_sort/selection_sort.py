24abd3cce1ed97e1ccad9146881c0e21db06b8b9
Pandula1234 <wpandulap@gmail.com>
2019-10-22 08:46:41 +0530

    Seletion sort is beign Modified    *********Team :- Binary Beasts

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,13 +1,20 @@
 def selection(a):
-        for i in range(0,len(a)-1):
+        for i in range(0,l):
                 minIndex=i
-                for j in range(i+1,len(a)):                        
-                        if a[minIndex]>a[j]:
+                for j in range(i+1,l):                        
+                        if arr[minIndex]>arr[j]:
                                 minIndex = j
                             
                 if minIndex != i:
-                    a[i],a[minIndex]=a[minIndex],a[i]
-        return a
-a=[1,88,2,56,56,5,56,2,5,65,8]
+                    arr[i],arr[minIndex]=arr[minIndex],arr[i]
+        return arr
+
+arr = []
+
+l=int(input("Enter Number Of Elements:"))
+for i in range(l):
+    a = int(input("enter num :"))
+    b = arr.append(a)
+        
 print(selection(a))
 
