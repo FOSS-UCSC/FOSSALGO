71e61d7c19bd4f8a5a6e6b5a5e05f4d538142bd3
Chathura Janaranjana Wanniarachchi <wachathuraj@gmail.com>
2019-10-22 22:03:23 +0530

    Update stack.c
    add pop function to remove elements from stack.

--- stack/stack.c
+++ stack/stack.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 void push(int);
-
+void pop();
 int stack[4];
 int size=sizeof(stack)/sizeof(int);
 int top=-1;
@@ -17,4 +17,13 @@ void push(int data){
 		stack[top]=data;
 	}else
 		printf("%d with Stack overflow\n",data);
+}
+
+//value deletion
+void pop(){
+	if(top>-1){
+		printf("%d ",stack[top]);
+		top=top-1;
+	}else
+		printf("\nStack underflow");
 }
