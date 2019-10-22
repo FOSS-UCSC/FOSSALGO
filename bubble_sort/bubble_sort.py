b4b8299ec9bbb1e104601f9fa7a2c807e5c351c9
Chathura Janaranjana Wanniarachchi <wachathuraj@gmail.com>
2019-10-22 21:32:10 +0530

    Update bubble_sort.py
    build code for sort given data set into ascending order.

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -3,3 +3,15 @@ count=int(input("Enter Count of the Numbers: "))
 dataSet = []
 for i in range(count):
     dataSet.append(int(input("enter num :")))
+out = 1
+print("Before Sort: ")
+print(dataSet)
+while out < count:
+    i = 1
+    while i < count:
+        if dataSet[i - 1] > dataSet[i]:
+            dataSet[i - 1], dataSet[i] = dataSet[i], dataSet[i - 1]
+        i = i + 1
+    out = out + 1
+print("After Sort: ")
+print(dataSet)
