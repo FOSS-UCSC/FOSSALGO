1e80fcf7318f10c7ba63436cc5c9fcc57f1ee336
Yasas Dilshan Ekanayaka <43106859+YasasDEK@users.noreply.github.com>
2019-10-22 23:38:25 +0530

    Update Lis.java

--- longest_increasing_subsequence/Lis.java
+++ longest_increasing_subsequence/Lis.java
@@ -1,63 +1,47 @@
-import java.util.Scanner;
-import java.util.Random;
 
-class Lis {
-	public static void main (String [] args) {
-		Scanner s = new Scanner(System.in);
-		Random r = new Random();
-		int N;
-		
-		do {			
-			System.out.print("Enter number of Invitations:\t");
-			N = s.nextInt();
-		} while (N<1);
+class LIS { 
+	static int max_ref; 
 
-		int [] arr = new int[N];
 
-		for (int i=0; i<N; i++) {
-			arr[i] = r.nextInt(N)+1;
-		}
+	static int _lis(int arr[], int n) 
+	{ 
+
+		if (n == 1) 
+			return 1; 
+
 		
-		System.out.print("Random Invitation Numbers  :\t");
-		for (int i=0; i<N; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println("");
-		
-		Lis l = new Lis();
-		l.lis(arr);
-		
-	}
+		int res, max_ending_here = 1; 
+
 	
-	public void lis (int [] arr) {
-		int [] lisLength = new int[arr.length];		//	stores length of longest increasing subsequence
-		int [] lisList = new int[arr.length];		//	keep track of longest increasing subsequence elements
-		int max = 1;								//	maXimum length of LIS
-		int maxIndex = 0;							//	maXimum length's index of lisList array
-		String lisPrint = "";						//	stores final output
+		for (int i = 1; i < n; i++) { 
+			res = _lis(arr, i); 
+			if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here) 
+				max_ending_here = res + 1; 
+		} 
+
 		
-		for (int i=0; i<arr.length; i++) {
-			lisLength[i] = 1;						//	initialize the lisLength array	(by setting 1)
-			lisList[i] = -1;						//	initialize the lisList array	(by setting NIL)
-			
-			for (int j=0; j<i; j++) {
-				if (arr[j] < arr[i] && lisLength[i] <= lisLength[j] + 1) {
-					lisLength[i] = lisLength[j] + 1;						//	increase LIS length
-					lisList[i] = j;											//	stores the index of corresponding LIS
-					if (max <= lisLength[i]) {
-						max = lisLength[i];									//	update the maX
-						maxIndex = i;
-					}
-				}
-			}
-		}
-		
-		for (int i=maxIndex; i>-1;) {
-			lisPrint = arr[i] + " " + lisPrint;			//	generate the output String from lisList array
-			i = lisList[i];
-		}
-		
-		System.out.println("increasing sequence Length :\t" + max);
-		System.out.println("increasing sequence\t   :\t" + lisPrint);
-	}
-}
+		if (max_ref < max_ending_here) 
+			max_ref = max_ending_here; 
+
+		return max_ending_here; 
+	} 
+
+	static int lis(int arr[], int n) 
+	{ 
+		max_ref = 1; 
+
+		_lis(arr, n); 
+
+		// returns max 
+		return max_ref; 
+	} 
+ 
+	public static void main(String args[]) 
+	{ 
+		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
+		int n = arr.length; 
+		System.out.println("Length of lis is "
+						+ lis(arr, n) + "\n"); 
+	} 
+} 
+/*This code is contributed by Rajat Mishra*/
