566e2161bb978e71d292f8c1c0564e053debde8f
Chathura Janaranjana Wanniarachchi <wachathuraj@gmail.com>
2019-10-22 14:19:57 +0530

    Update bubble_sort.py
    add two while loop to sort the numbers for ascending order.

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -4,6 +4,17 @@ array = []
 for i in range(dataCount):
     array.append(int(input("enter num :")))
 
-print("Unsorted Dataset")
-for j in array:
-    print(j ,end=" ")
+print("Unsorted Dataset: ")
+print(array)
+
+count = len(array)
+out = 1
+while out < count:
+    i = 1
+    while i < count:
+        if array[i - 1] > array[i]:
+            array[i - 1], array[i] = array[i], array[i - 1]
+        i = i + 1
+    out = out + 1
+print("Sorted Dataset: ")
+print(array)
