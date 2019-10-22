13ccf29161dcf46b6b036519ad2a867a228b1022
Tamashasen <tamzsene@gmail.com>
2019-10-22 23:10:41 +0530

    Update bubbleSort.java

--- bubble_sort/bubbleSort.java
+++ bubble_sort/bubbleSort.java
@@ -1,35 +1,36 @@
-import java.util.Scanner;
+class BubbleSort 
+{ 
+	void bubbleSort(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i = 0; i < n-1; i++) 
+			for (int j = 0; j < n-i-1; j++) 
+				if (arr[j] > arr[j+1]) 
+				{ 
+					 
+					int temp = arr[j]; 
+					arr[j] = arr[j+1]; 
+					arr[j+1] = temp; 
+				} 
+	} 
 
-class bubbleSort{
-	public static void main(String a[]){
+	
+	void printArray(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i=0; i<n; ++i) 
+			System.out.print(arr[i] + " "); 
+		System.out.println(); 
+	} 
 
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Size of array :");
-		int n=sc.nextInt();
+	 
+	public static void main(String args[]) 
+	{ 
+		BubbleSort ob = new BubbleSort(); 
+		int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+		ob.bubbleSort(arr); 
+		System.out.println("Sorted array"); 
+		ob.printArray(arr); 
+	} 
+} 
 
-		int[] num=new int[n];
-		int i,j,tmp;
-
-		for(i=0;i<n;i++){
-			num[i]=sc.nextInt();
-		}
-		
-		for(i=0;i<n;i++){
-			for(j=0;j<(n-i);j++){
-				if(j==(n-1)){
-					break;
-				}
-				else{
-					if(num[j]>num[j+1]){
-						tmp=num[j];
-						num[j]=num[j+1];
-						num[j+1]=tmp;
-					}
-				}
-			}
-		}
-		for(i=0;i<n;i++){
-			System.out.print(num[i]+"  "  );
-		}
-	}
-}
