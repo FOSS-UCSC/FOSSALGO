402f481c08668b6bdb061b7b587c35f2ad66b7c2
Pandula1234 <wpandulap@gmail.com>
2019-10-22 23:55:06 +0530

    For Loop IS beign Modified

--- naive_search/Naive_search.cpp
+++ naive_search/Naive_search.cpp
@@ -7,12 +7,12 @@ int match(char *txt,char *pat){
     int n=strlen(txt);
     int m=strlen(pat);
     int i,j,k;
-    for(i=0;i<(n-m+1);i++){
+    for(int i = 0; i <= N - M; i++) { 
 	//check patern match for i
         for(j=0;j<=m-1;j++){
             if(txt[i+j]!=pat[j])
                 break;
-        if(j==m-1){
+        if(j==m){
            printf("Pattern found at %d\n",i);
         }
         }
