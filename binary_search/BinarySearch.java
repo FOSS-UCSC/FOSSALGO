7e3cfc47cca0f64a6787c5c8123347c05f1ca5ad
Nandula Perera <pereranandula1999@gmail.com>
2019-10-22 19:59:27 +0530

    Added Binary Search in C and Java

---
+++ binary_search/BinarySearch.java
@@ -0,0 +1,42 @@
+public class BinarySearch {
+
+    public static void sort(int arr[],int n){
+        int i,j;
+        for(i=0;i<n;i++){
+            for(j=0;j<n-1;j++){
+                if(arr[j]>=arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static int binarySearch(int arr[],int low,int high,int searchVal){
+        if(low>high){
+            System.out.println("Search is finished. Value not found");
+            return -1;
+        }
+        int mid=(low+high)/2;
+        if(searchVal==arr[mid]){
+            System.out.println("Search is finished. Index is "+mid);
+            return mid;
+        }else if(searchVal>=arr[mid]){
+            low=mid+1;
+            return binarySearch(arr,low,high,searchVal);
+        }else{
+            high=mid-1;
+            return binarySearch(arr,low,high,searchVal);
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[]={4,5,1,10,2,7,6,3};
+        sort(arr,arr.length);
+        int low=0;
+        binarySearch(arr,low,arr.length-1,7);
+        binarySearch(arr,low,arr.length-1,8);
+    }
+
+}
