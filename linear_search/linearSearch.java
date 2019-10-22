d966268aef45c16ea63b8bd086e58c5122a7c06d
Uthpala Ruwanara <uruwanara@users.noreply.github.com>
2019-10-22 23:32:02 +0530

    Update linearSearch.java

--- linear_search/linearSearch.java
+++ linear_search/linearSearch.java
@@ -1,16 +1,45 @@
-class linearSearch
-{
-    
-    static int search(int arr[], int n, int x)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            
-            if (arr[i] == x)
+public class linearSearch {
+
+    public static final int unorderedLinearSearch(int value, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int iValue = array[i];
+            if (value == iValue)
                 return i;
         }
- 
-        
-        return -1;
+        return Integer.MAX_VALUE;
     }
-} 
+    
+    public static final int orderedLinearSearch(int value, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (value == array[i]){
+                return i;
+            }
+            else if (array[i] > value){
+             return -1;
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+    
+    public static void main(String[] args) {
+
+
+      int[] integers = {1,2,3,4,5,6,7,8,8,8,9,9,9,0};
+         int shouldBeFound = 9;
+
+         int atIndex = linearSearch.unorderedLinearSearch(shouldBeFound, integers);
+
+         System.out.println(String.format("Should be found: %d. Found %d at index %d. An array length %d"
+                 , shouldBeFound, integers[atIndex], atIndex, integers.length));
+         
+         int[] sortedArray = {10,20,30,40,50};
+         //the element that should be found
+         int key = 30;
+         
+         atIndex = linearSearch.orderedLinearSearch(key, sortedArray);
+
+         System.out.println(String.format("Should be found: %d. Found %d at index %d. An array length %d"
+                 , key, sortedArray[atIndex], atIndex, sortedArray.length));
+         
+ }
+}
