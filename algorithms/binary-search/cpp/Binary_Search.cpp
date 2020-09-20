de2b4ec8a7e0d97af4c1f264a5f5058a1e053bc1
danushka96 <danushkaherath96@gmail.com>
2020-09-20 18:50:07 +0530

    added golang build

--- algorithms/binary-search/cpp/Binary_Search.cpp
+++ algorithms/binary-search/cpp/Binary_Search.cpp
@@ -1,37 +1,39 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int elements,binarySearch[100],i,result,value,searchValue,j,k,l;
-	cout<<"Enter The Elements:";
-	cin>>elements;
-	cout<<"Enter "<<elements<< " Of Elements:";
+//todo some errors occured at compiling
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// 	int elements,binarySearch[100],i,result,value,searchValue,j,k,l;
+// 	cout<<"Enter The Elements:";
+// 	cin>>elements;
+// 	cout<<"Enter "<<elements<< " Of Elements:";
 	
-	for(i=0;i<elements;i++){
-		cin>>value;
-		binarySearch[i]=value;
-	}
-	cout<<"\n Enter The Element to Search:";
-	cin>>searchValue;
-	j=0;
-	k=elements-1;
-	int count=0;
-	while(j<=k){
-		l=(j+k)/2;
-		if(searchValue==binarySearch[l]){
-			count++;
-			cout<<"Element is found !!!";
-			break;
-		}else{
-			if(searchValue>binarySearch[l]){
-				j=l+1;
-			}else{
-				k=l-1;
-			}
-		}
-	}
-	if(count==0){
-	printf("Element is not found!!!");	
-	}
+// 	for(i=0;i<elements;i++){
+// 		cin>>value;
+// 		binarySearch[i]=value;
+// 	}
+// 	cout<<"\n Enter The Element to Search:";
+// 	cin>>searchValue;
+// 	j=0;
+// 	k=elements-1;
+// 	int count=0;
+// 	while(j<=k){
+// 		l=(j+k)/2;
+// 		if(searchValue==binarySearch[l]){
+// 			count++;
+// 			cout<<"Element is found !!!";
+// 			break;
+// 		}else{
+// 			if(searchValue>binarySearch[l]){
+// 				j=l+1;
+// 			}else{
+// 				k=l-1;
+// 			}
+// 		}
+// 	}
+// 	if(count==0){
+// 	printf("Element is not found!!!");	
+// 	}
 	
-	return 0;
-}
+// 	return 0;
+// }
