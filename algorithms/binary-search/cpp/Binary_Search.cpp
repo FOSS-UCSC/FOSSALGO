43c7e20681433f72c6a9d9166ee671a10ecdf3c3
danushka96 <danushkaherath96@gmail.com>
2020-09-20 19:39:32 +0530

    updated c++ code

--- algorithms/binary-search/cpp/Binary_Search.cpp
+++ algorithms/binary-search/cpp/Binary_Search.cpp
@@ -1,39 +1,81 @@
-//todo some errors occured at compiling
-
-// #include<iostream>
-// using namespace std;
-// int main(){
-// 	int elements,binarySearch[100],i,result,value,searchValue,j,k,l;
-// 	cout<<"Enter The Elements:";
-// 	cin>>elements;
-// 	cout<<"Enter "<<elements<< " Of Elements:";
-	
-// 	for(i=0;i<elements;i++){
-// 		cin>>value;
-// 		binarySearch[i]=value;
-// 	}
-// 	cout<<"\n Enter The Element to Search:";
-// 	cin>>searchValue;
-// 	j=0;
-// 	k=elements-1;
-// 	int count=0;
-// 	while(j<=k){
-// 		l=(j+k)/2;
-// 		if(searchValue==binarySearch[l]){
-// 			count++;
-// 			cout<<"Element is found !!!";
-// 			break;
-// 		}else{
-// 			if(searchValue>binarySearch[l]){
-// 				j=l+1;
-// 			}else{
-// 				k=l-1;
-// 			}
-// 		}
-// 	}
-// 	if(count==0){
-// 	printf("Element is not found!!!");	
-// 	}
-	
-// 	return 0;
-// }
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+   int key;
+   struct node *left, *right;
+};
+struct node *newNode(int item){
+   struct node *temp = (struct node *)malloc(sizeof(struct node));
+   temp->key = item;
+   temp->left = temp->right = NULL;
+   return temp;
+}
+void inordertraversal(struct node *root){
+   if (root != NULL){
+      inordertraversal(root->left);
+      printf("%d ", root->key);
+      inordertraversal(root->right);
+   }
+}
+struct node* insert(struct node* node, int key){
+   if (node == NULL) return newNode(key);
+      if (key < node->key)
+         node->left = insert(node->left, key);
+      else
+         node->right = insert(node->right, key);
+   return node;
+}
+struct node * minValueNode(struct node* node){
+   struct node* current = node;
+   while (current && current->left != NULL)
+      current = current->left;
+   return current;
+}
+struct node* deleteNode(struct node* root, int key){
+   if (root == NULL) return root;
+      if (key < root->key)
+         root->left = deleteNode(root->left, key);
+      else if (key > root->key)
+         root->right = deleteNode(root->right, key);
+   else{
+      if (root->left == NULL){
+         struct node *temp = root->right;
+         free(root);
+         return temp;
+      }
+      else if (root->right == NULL){
+         struct node *temp = root->left;
+         free(root);
+         return temp;
+      }
+      struct node* temp = minValueNode(root->right);
+      root->key = temp->key;
+      root->right = deleteNode(root->right, temp->key);
+   }
+   return root;
+}
+int main(){
+   struct node *root = NULL;
+   root = insert(root, 50);
+   root = insert(root, 30);
+   root = insert(root, 20);
+   root = insert(root, 40);
+   root = insert(root, 70);
+   root = insert(root, 60);
+   root = insert(root, 80);
+   printf("Inorder traversal of the given tree \n");
+   inordertraversal(root);
+   printf("\nDelete 20\n");
+   root = deleteNode(root, 20);
+   printf("Inorder traversal of the modified tree \n");
+   inordertraversal(root);
+   printf("\nDelete 30\n");
+   root = deleteNode(root, 30);
+   printf("Inorder traversal of the modified tree \n");
+   inordertraversal(root);
+   printf("\nDelete 50\n");
+   root = deleteNode(root, 50);
+   printf("Inorder traversal of the modified tree \n");
+   inordertraversal(root);
+   return 0;
+}
