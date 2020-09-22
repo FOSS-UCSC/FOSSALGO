78caaf9a3c8dfd9b200f85567af00729997aac1a
Sachintha <ravindusachintha53@gmail.com>
2020-09-22 15:18:46 +0530

    updates for automation (#361)
    Updated automation of safety-checks and program compilations by @RavinduSachintha, @Danushka96

--- binary_search_tree/BST.java
+++ binary_search_tree/BST.java
@@ -10,7 +10,7 @@ public class BST {
 	public void insert(int data){
 		if (this.root == null) {
 			this.root = new Node(data);
-		}else{ghjfgj
+		}else{
 			this._insert(data, this.root);
 		}
 		count++;
