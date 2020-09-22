1de1f96f9779d540058ee60124d833d92c0e5c9a
Ravindu Sachintha <ravindusachintha53@gmail.com>
2020-09-22 09:07:57 +0530

    file structure safety checker updated for level wise validation

--- safety-checkers/file-structure.sh
+++ safety-checkers/file-structure.sh
@@ -1,1 +1,55 @@
-exit 3
+# global variables
+arrLanguages=()
+
+function exitWithError() {
+  echo "Validation failed : $1"
+  exit 1
+}
+
+while read F ; do
+    arrLanguages+=($F)
+done < ./language-list.txt
+
+for i in {1..3} ; do
+  echo "-> Folder Level $i Validation:"
+
+  # validation for algorithms folder
+  for d in `find ../algorithms -mindepth $i -maxdepth $i` ; do
+
+    # ignore .gitignore & .gitkeep files
+    if [ ${d: -8} == ".gitkeep" ] || [ ${d: -9} == "README.md" ] || [ ${d: -10} == ".gitignore" ] ; then
+      continue
+    fi
+
+    # 1st level folder name validation (lowercase & hyphen only)
+    if [ $i == 1 ] && ! [[ $d =~ ^../algorithms/[a-z]+(-[a-z]+)*$ ]] ; then
+      exitWithError "$d is not a valid folder name"
+    fi
+
+    # 2nd level folder name validation (lowercase, numbers only)
+    if [ $i == 2 ] ; then
+      if ! [[ $d =~ ^../algorithms/[a-z]+(-[a-z]+)/[a-z0-9]+$ ]] ; then
+        exitWithError "$d is not a valid language folder name"
+      fi
+
+      IFS='/' read -r -a arrPath <<< $d
+      if [[ " ${arrLanguages[*]} " != *" ${arrPath[3]} "* ]]; then
+        exitWithError "$d is not a defined language folder name"
+      fi
+    fi
+
+    # 3rd level file name validation (lowercase, numbers only)
+    if [ $i == 3 ] ; then
+
+      IFS='/' read -r -a arrPath <<< $d
+
+      # file name validation for upper-camel (pascal) case
+      if [ ${arrPath[3]} == "java" ] && ! [[ ${arrPath[4]} =~ ^([A-Z][a-z0-9]+)+.java$ ]]; then
+          exitWithError "$d is not in a valid naming convention"
+      fi
+    fi
+
+  done
+
+  echo -e "Validation passed!"
+done
