e2c0434b8f95f9cc04990d2382b28693c9c8e5d3
Ravindu Sachintha <ravindusachintha53@gmail.com>
2020-09-28 13:06:05 +0530

    doubly_linked_list.py issues fixed

--- data-structures/ds-dlinklst/python3/doubly_linked_list.py
+++ data-structures/ds-dlinklst/python3/doubly_linked_list.py
@@ -1,46 +1,53 @@
-class Node:    
-    def __init__(self,data):    
-        self.data = data;    
-        self.previous = None;    
-        self.next = None;    
-            
-class DoublyLinkedList:    
-    def __init__(self):    
-        self.head = None;    
-        self.tail = None;    
-            
-  
-    def addNode(self, data):
-        newNode = Node(data);    
-                
-        if(self.head == None):    
-            self.head = self.tail = newNode;    
-            self.head.previous = None;    
-            self.tail.next = None;    
-        else:    
-            self.tail.next = newNode;    
-            newNode.previous = self.tail;    
-            self.tail = newNode;    
-            self.tail.next = None;    
-                
-    def display(self):    
-        current = self.head;    
-        if(self.head == None):    
-            print("Doubly Link List is empty");    
-            return;    
-        print("Nodes");    
-        while(current != None):     
-            print(current.data),;    
-            current = current.next;    
-                
-dList = DoublyLinkedList();    
+#!/usr/bin/env python3
+"""Python3 script for doubly linked list data structure"""
 
 
-dList.addNode(54);    
-dList.addNode(27);    
-dList.addNode(76);    
-dList.addNode(65);    
-dList.addNode(4);    
-     
+class Node(object):
+    """Node for the linked list"""
+    def __init__(self, data):
+        self.data = data
+        self.previous = None
+        self.next = None
 
-dList.display();  
+
+class DoublyLinkedList(object):
+    """Doubly linked list implementation"""
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_node(self, data):
+        """Add a new node to the linked list"""
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = self.tail = new_node
+            self.head.previous = None
+            self.tail.next = None
+        else:
+            self.tail.next = new_node
+            new_node.previous = self.tail
+            self.tail = new_node
+            self.tail.next = None
+
+    def display(self):
+        """Display items of the linked list"""
+        current = self.head
+        if self.head is None:
+            print("Doubly Link List is empty")
+            return
+        print("Nodes")
+        while current is not None:
+            print(current.data),
+            current = current.next
+
+
+DLIST = DoublyLinkedList()
+
+DLIST.add_node(54)
+DLIST.add_node(27)
+DLIST.add_node(76)
+DLIST.add_node(65)
+DLIST.add_node(4)
+
+DLIST.display()
