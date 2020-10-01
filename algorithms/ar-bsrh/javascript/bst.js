819c77c36b14f01abd8cc2244a7d53ce32acd50b
Ravindu Sachintha <ravindusachintha53@gmail.com>
2020-10-01 07:29:05 +0530

    ar-bsrh issues fixed

--- algorithms/ar-bsrh/javascript/bst.js
+++ algorithms/ar-bsrh/javascript/bst.js
@@ -2,132 +2,151 @@ class Node {
     constructor(data) {
         this.right = null;
         this.left = null;
-        this.data = data
+        this.data = data;
     }
 }
 
-
 class Bst {
     constructor() {
-        this.root = null
+        this.root = null;
     }
 
     insert(data) {
         var node = new Node(data);
         if (!this.root) {
             this.root = node;
-            return this
+            return this;
         }
-        let current = this.root
+        let current = this.root;
         while (current) {
             if (data === current.data) {
                 return;
             }
             if (data < current.data) {
-
                 if (!current.left) {
-                    current.left = node
-                    break
+                    current.left = node;
+                    break;
                 }
-                current = current.left
+                current = current.left;
             }
             if (data > current.data) {
-
                 if (!current.right) {
-                    current.right = node
-                    break
+                    current.right = node;
+                    break;
                 }
-                current = current.right
+                current = current.right;
             }
         }
-        return this
+        return this;
     }
     
     find(data) {
-        if (!this.root) return null
+        if (!this.root) {
+            return null;
+        }
         let current = this.root;
         while (current) {
-            if (data == current.data) return current.data;
+            if (data == current.data) {
+                return current.data;
+            }
             if (current.right && data > current.data) {
-                current = current.right
+                current = current.right;
             } else {
-                current = current.left
+                current = current.left;
             }
         }
-        return false
+        return false;
     }
     
     contains(data) {
-        const found = this.find(data)
+        const found = this.find(data);
         if (found) {
-            return true
+            return true;
         }
-        return false
+        return false;
     }
     bfs() {
         let node = this.root;
         const queue = [node];
         const finalData = []
         while (queue.length) {
-            node = queue.shift()
-            if (node.left) queue.push(node.left)
-            if (node.right) queue.push(node.right)
-            finalData.push(node.data)
+            node = queue.shift();
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+            finalData.push(node.data);
         }
-        return finalData
+        return finalData;
     }
 
     preOrder() {
         const finalData = [];
         function traverse(node) {
-            finalData.push(node.data)
-            if (node.left) traverse(node.left)
-            if (node.right) traverse(node.right)
+            finalData.push(node.data);
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
         }
-        traverse(this.root)
-        return finalData
+        traverse(this.root);
+        return finalData;
     }
 
     postOrder() {
         const finalData = [];
         function traverse(node) {
-            if (node.left) traverse(node.left)
-            if (node.right) traverse(node.right)
-            finalData.push(node.data)
+            if (node.left) {
+                traverse(node.left);
+            }
+            if (node.right) {
+                traverse(node.right);
+            }
+            finalData.push(node.data);
         }
 
-        traverse(this.root)
-        return finalData
+        traverse(this.root);
+        return finalData;
     }
 
     inOrder() {
         const finalData = [];
         function traverse(node) {
-            if (node.left) traverse(node.left)
-            finalData.push(node.data)
-            if (node.right) traverse(node.right)
+            if (node.left) {
+                traverse(node.left);
+            }
+            finalData.push(node.data);
+            if (node.right) {
+                traverse(node.right);
+            }
         }
 
-        traverse(this.root)
-        return finalData
+        traverse(this.root);
+        return finalData;
     }
 
     maxNode() {
         if (!this.root) return null;
         let current = this.root;
         while (current.right) {
-            current = current.right
+            current = current.right;
         }
-        return current.data
+        return current.data;
     }
 
     minNode() {
-        if (!this.root) return null
-        let current = this.root
+        if (!this.root) {
+            return null;
+        }
+        let current = this.root;
         while (current.left) {
-            current = current.left
+            current = current.left;
         }
 
-        return current.data
+        return current.data;
     }
 }
