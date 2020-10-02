7f4d9029c72f418e549801e0ff4be9328c90e0ad
Srinjoy Bhuiya <srinjoybhuiya@gmail.com>
2020-10-03 01:23:50 +0530

    Added the binary search algorithm in javascript  (#383)
    * Added the binary search algorithm in javascript
    
    * Added the binary search algorithm for javascript
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo
    
    * Update README.md
    
    * Update README.md

---
+++ algorithms/ar-bsrh/javascript/binary_search.js
@@ -0,0 +1,37 @@
+function binarySearch(items, value) {
+	var firstIndex = 0,
+		lastIndex = items.length - 1,
+		middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+
+	while (firstIndex <= lastIndex) {
+		let middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+		if (items[middleIndex] === value) {
+			return middleIndex;
+		}
+		else if (value < items[middleIndex]) {
+			lastIndex = middleIndex - 1;
+		}
+		else {
+			firstIndex = middleIndex + 1;
+		}
+		middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+	}
+
+	return items[middleIndex] !== value ? -1 : middleIndex;
+}
+function main() {
+	let items = [
+		1,
+		2,
+		3,
+		4,
+		5,
+		7,
+		8,
+		9
+	];
+	binarySearch(items, 1);
+	binarySearch(items, 5);
+}
+
+main();
