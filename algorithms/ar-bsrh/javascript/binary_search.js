e7d705239ad94f1519c37c9812be4fcc2a769ed5
Srinjoycode <srinjoybhuiya@gmail.com>
2020-10-02 22:01:09 +0530

    added the binary search algorithm in javascript in accordance with the syntaxing rules of the repo

--- algorithms/ar-bsrh/javascript/binary_search.js
+++ algorithms/ar-bsrh/javascript/binary_search.js
@@ -3,14 +3,15 @@ function binarySearch(items, value) {
 		lastIndex = items.length - 1,
 		middleIndex = Math.floor((lastIndex + firstIndex) / 2);
 
-	while (items[middleIndex] !== value && firstIndex < lastIndex) {
-		if (value < items[middleIndex]) {
+	while (firstIndex < lastIndex) {
+		let middleIndex = Math.floor((lastIndex + firstIndex) / 2);
+		if (items[middleIndex] === value) return middleIndex;
+		else if (value < items[middleIndex]) {
 			lastIndex = middleIndex - 1;
 		}
 		else if (value > items[middleIndex]) {
 			firstIndex = middleIndex + 1;
 		}
-		middleIndex = Math.floor((lastIndex + firstIndex) / 2);
 	}
 
 	return items[middleIndex] !== value ? -1 : middleIndex;
