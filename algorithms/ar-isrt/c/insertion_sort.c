e587b5feb676d0001e3f1acd69e33376fbd43bbe
ashera96 <asherasilva100@gmail.com>
2020-10-03 13:14:34 +0530

    Fixes

--- algorithms/ar-isrt/c/insertion_sort.c
+++ algorithms/ar-isrt/c/insertion_sort.c
@@ -2,10 +2,10 @@
 
 // Insertion sort function
 void insertionSort(int arr[], int len) {
-	int i, j, key;
+	int i, key;
 	for (i=1 ; i<len ; i++) {
+		int j = i-1;
 		key = arr[i];
-		j = i-1;
 		while (j>=0 && arr[j]>key) {
 			arr[j+1] = arr[j];
 			j-=1;
