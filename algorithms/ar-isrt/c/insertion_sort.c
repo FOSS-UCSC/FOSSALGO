b40784e168758a327bc63cf2468dace890499c91
ashera96 <asherasilva100@gmail.com>
2020-10-03 12:54:46 +0530

    Insertion Sort in C

---
+++ algorithms/ar-isrt/c/insertion_sort.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+
+// Insertion sort function
+void insertionSort(int arr[], int len) {
+	int i, j, key;
+	for (i=1 ; i<len ; i++) {
+		key = arr[i];
+		j = i-1;
+		while (j>=0 && arr[j]>key) {
+			arr[j+1] = arr[j];
+			j-=1;
+		}
+		arr[j+1] = key;
+	}
+}
+
+// Function to print an array
+void printArray(int arr[], int len) {
+	int i;
+	for (i=0 ; i<len ; i++)
+		printf("%d ", arr[i]);\
+	printf("\n");
+}
+
+// Function to test insetion sort
+int main() {
+	int arr[] = {22, 11, 8, 5, 30};
+	int len = sizeof(arr)/sizeof(arr[0]);
+	insertionSort(arr, len);
+	printArray(arr, len);
+	return 0;
+}
