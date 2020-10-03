a5a1c21c984d59cc0fb9300cd0a33878cdd7b9a7
DHATRISH AVS <dhathrishu.1947@gmail.com>
2020-10-03 14:26:27 +0530

    Create binary_search.py
    Updated binary_search

--- algorithms/ar-bsrh/python2/binary_search.py
+++ algorithms/ar-bsrh/python2/binary_search.py
@@ -2,20 +2,14 @@ def binarysearch(arr,x):
   low=0
   high=len(arr)-1
   mid=0
-
-  while (low<=high):
-    
+  while (low<=high): 
     mid=(low+high)//2
-    
     if (arr[mid]<x):
       low=mid+1
-    
     elif (arr[mid]>x):
       high=mid-1
-    
     else: 
       return mid
-  
   return -1
   
 def main():
@@ -28,3 +22,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
