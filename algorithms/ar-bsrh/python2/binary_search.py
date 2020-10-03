9585e8cde5f5f747276654146331652af3955435
DHATRISH AVS <dhathrishu.1947@gmail.com>
2020-10-03 14:49:22 +0530

    Create binary_search in python 2

--- algorithms/ar-bsrh/python2/binary_search.py
+++ algorithms/ar-bsrh/python2/binary_search.py
@@ -2,29 +2,22 @@ def binarysearch(arr,x):
   low=0
   high=len(arr)-1
   mid=0
-
-  while (low<=high):
-    
+  while(low<=high):
     mid=(low+high)//2
-    
     if (arr[mid]<x):
       low=mid+1
-    
     elif (arr[mid]>x):
       high=mid-1
-    
     else: 
       return mid
-  
   return -1
-  
 def main():
   arr = [1,2,3,4,5,6]
   index = binarysearch(arr,5)
-  if (index==-1):
+  if(index==-1):
     print "The element is not found!"
   else:
     print "Element is found at index:",index
-
 if __name__ == '__main__':
     main()
+    
