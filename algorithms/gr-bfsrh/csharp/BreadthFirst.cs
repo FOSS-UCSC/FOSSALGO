adbee1ca3280a3ad48d4b988e43fdec0e9373237
Danushka Herath <Danushkaherath96@gmail.com>
2020-10-11 20:54:57 +0530

    Fixed BreadthFirstSearch compiler issue (#476)
    * changed go execution
    
    * test commit
    
    * added c# compiler
    
    * added c# compiler
    
    * fixed BreadthFirst code compile issue

--- algorithms/gr-bfsrh/csharp/BreadthFirst.cs
+++ algorithms/gr-bfsrh/csharp/BreadthFirst.cs
@@ -1,95 +1,95 @@
-using System.Collections.Generic;
+using System; 
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
 
-namespace Algorithms
-{
-    //Based on https://www.geeksforgeeks.org/shortest-distance-two-cells-matrix-grid/
-    //Binary-first search
-    public static class BreadthFirst
-    {
-        private const int BLOCKED = 1;
-        private const int START = 2;
-        private const int END = 3;
+// This class represents a directed 
+// graph using adjacency list 
+// representation 
+class Graph{ 
+	
+// No. of vertices	 
+private int _V; 
 
-        private static int MinDistance(int[][] grid)
-        {
-            Node source = new Node(0, 0, 0);
+//Adjacency Lists 
+LinkedList<int>[] _adj; 
 
-            var n = grid.Length;
-            var m = grid[0].Length;
+public Graph(int V) 
+{ 
+	_adj = new LinkedList<int>[V]; 
+	for(int i = 0; i < _adj.Length; i++) 
+	{ 
+		_adj[i] = new LinkedList<int>(); 
+	} 
+	_V = V; 
+} 
 
-            // To keep track of visited QItems. Marking 
-            // blocked cells as visited. 
-            bool[][] visited = TrackVisited(grid, source, n, m);
+// Function to add an edge into the graph 
+public void AddEdge(int v, int w) 
+{		 
+	_adj[v].AddLast(w); 
 
-            // applying BFS on matrix cells starting from source 
-            Queue<Node> q = new Queue<Node>();
-            q.Enqueue(source);
-            visited[source.X][source.Y] = true;
-            while (q.Count > 0)
-            {
-                var p = q.Dequeue();
+} 
 
-                if (grid[p.X][p.Y] == END)
-                {
-                    return p.DistanceFromSource;
-                }
+// Prints BFS traversal from a given source s 
+public void BFS(int s) 
+{ 
+	
+	// Mark all the vertices as not 
+	// visited(By default set as false) 
+	bool[] visited = new bool[_V]; 
+	for(int i = 0; i < _V; i++) 
+		visited[i] = false; 
+	
+	// Create a queue for BFS 
+	LinkedList<int> queue = new LinkedList<int>(); 
+	
+	// Mark the current node as 
+	// visited and enqueue it 
+	visited[s] = true; 
+	queue.AddLast(s);		 
 
-                AddNeighbors(n, m, visited, q, p);
-            }
-            return -1;
-        }
+	while(queue.Any()) 
+	{ 
+		
+		// Dequeue a vertex from queue 
+		// and print it 
+		s = queue.First(); 
+		Console.Write(s + " " ); 
+		queue.RemoveFirst(); 
+		
+		// Get all adjacent vertices of the 
+		// dequeued vertex s. If a adjacent 
+		// has not been visited, then mark it 
+		// visited and enqueue it 
+		LinkedList<int> list = _adj[s]; 
 
-        private static bool[][] TrackVisited(int[][] grid, Node source, int n, int m)
-        {
-            bool[][] visited = new bool[n][];
-            for (int i = 0; i < n; i++)
-            {
-                visited[i] = new bool[m];
-                for (int j = 0; j < m; j++)
-                {
-                    visited[i][j] = grid[i][j] == BLOCKED;
+		foreach (var val in list)			 
+		{ 
+			if (!visited[val]) 
+			{ 
+				visited[val] = true; 
+				queue.AddLast(val); 
+			} 
+		} 
+	} 
+} 
 
-                    // Finding source 
-                    if (grid[i][j] == START)
-                    {
-                        source.X = i;
-                        source.Y = j;
-                    }
-                }
-            }
-
-            return visited;
-        }
-
-        private static void AddNeighbors(int n, int m, bool[][] visited, Queue<Node> q, Node p)
-        {
-            // moving up 
-            if (p.X - 1 >= 0 && visited[p.X - 1][p.Y] == false)
-            {
-                q.Enqueue(new Node(p.X - 1, p.Y, p.DistanceFromSource + 1));
-                visited[p.X - 1][p.Y] = true;
-            }
-
-            // moving down 
-            if (p.X + 1 < n && visited[p.X + 1][p.Y] == false)
-            {
-                q.Enqueue(new Node(p.X + 1, p.Y, p.DistanceFromSource + 1));
-                visited[p.X + 1][p.Y] = true;
-            }
-
-            // moving left 
-            if (p.Y - 1 >= 0 && visited[p.X][p.Y - 1] == false)
-            {
-                q.Enqueue(new Node(p.X, p.Y - 1, p.DistanceFromSource + 1));
-                visited[p.X][p.Y - 1] = true;
-            }
-
-            // moving right 
-            if (p.Y + 1 < m && visited[p.X][p.Y + 1] == false)
-            {
-                q.Enqueue(new Node(p.X, p.Y + 1, p.DistanceFromSource + 1));
-                visited[p.X][p.Y + 1] = true;
-            }
-        }
-    }
-}
+// Driver code 
+static void Main(string[] args) 
+{ 
+	Graph g = new Graph(4); 
+	
+	g.AddEdge(0, 1); 
+	g.AddEdge(0, 2); 
+	g.AddEdge(1, 2); 
+	g.AddEdge(2, 0); 
+	g.AddEdge(2, 3); 
+	g.AddEdge(3, 3); 
+	
+	Console.Write("Following is Breadth First " + 
+				"Traversal(starting from " + 
+				"vertex 2)\n"); 
+	g.BFS(2); 
+} 
+} 
