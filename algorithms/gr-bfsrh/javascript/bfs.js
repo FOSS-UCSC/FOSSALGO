83d8f3bbdc99325e925c06d1426fb8c6441d594f
valentina <valentina.calabrese@cloudacademy.com>
2020-10-12 20:48:44 +0200

    refactoring js

--- algorithms/gr-bfsrh/javascript/bfs.js
+++ algorithms/gr-bfsrh/javascript/bfs.js
@@ -1,47 +1,20 @@
-const bfs = (graph, root, targetNode) => {
+function bfs(value, tree) {
+  queue = [];
 
-  buildPath = (parents, targetNode) => {
-    let result = [targetNode];
-    while (parents[targetNode] !== null) {
-      targetNode = parents[targetNode];
-      result.push(targetNode);
-    }
-    return result.reverse();
-  };
+  queue.unshift(tree);
 
-  let parents = [];
-  let queue = [];
-  let visited = [];
-  let current;
-
-  queue.push(root);
-  parents[root] = null;
-  visited[root] = true; // mark first node as visited
-
-  while(queue.length !== -1) {
-    current = queue.shift();
-
-    if (current === targetNode) {
-      return buildPath(parents, targetNode);
+  while (queue.length > 0) {
+    curNode = queue.pop();
+    if (curNode.value === value) {
+      return curNode;
     }
 
-    for (let i = 0; i < graph.length; i++) {
-      if (i !== current && graph[current][i] && !visited[i]) {
-        parents[i] = current;
-        visited[i] = true;
-        queue.push(i);
-      }
+    var len = curNode.children.length;
+
+    for (var i = 0; i < len; i++) {
+      queue.unshift(curNode.children[i]);
     }
   }
+
   return null;
-};
-
-// Test Example
-const graph = [[1, 1, 0, 0, 1, 0],
-             [1, 0, 1, 0, 1, 0],
-             [0, 1, 0, 1, 0, 0],
-             [0, 0, 1, 0, 1, 1],
-             [1, 1, 0, 1, 0, 0],
-             [0, 0, 0, 1, 0, 0]];
-
-const shortestPath = bfs(graph, 1, 5); // [1, 2, 3, 5]
+}
