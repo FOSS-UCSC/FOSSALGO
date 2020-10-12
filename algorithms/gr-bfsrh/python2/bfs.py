adbc6b2e9969335961a50f3c3d1a254c8104515d
Vale <vale@MacBook-Pro.local>
2020-10-12 09:59:23 +0200

    fixing linting issues

--- algorithms/gr-bfsrh/python2/bfs.py
+++ algorithms/gr-bfsrh/python2/bfs.py
@@ -19,24 +19,21 @@ class Graph:
 
     while queue:
       s = queue.pop(0)
-      print (s, end = " ")
-
 
       for i in self.graph[s]:
         if visited[i] == False:
           queue.append(i)
           visited[i] = True
 
-g = Graph() 
-g.addEdge(0, 1) 
-g.addEdge(0, 2) 
-g.addEdge(1, 2) 
-g.addEdge(2, 0) 
-g.addEdge(2, 3) 
-g.addEdge(3, 3) 
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
 
 
-print ("Breadth First Traversal"
-                  " (starting from vertex 2)") 
+print 'Breadth First Traversal (starting from vertex 2)'
 g.BFS(2)
 
