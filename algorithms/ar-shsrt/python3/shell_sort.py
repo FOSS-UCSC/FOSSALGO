fa7c2b2d0ab55b77a55ac6931fce469af48196cc
charindukavishan <charindukavishan@gmail.com>
2020-10-12 17:27:32 +0530

    removed trailing whitespace

--- algorithms/ar-shsrt/python3/shell_sort.py
+++ algorithms/ar-shsrt/python3/shell_sort.py
@@ -1,21 +1,21 @@
-def shellSort(arr):    
-    n = len(arr)     
+def shellSort(arr):
+    n = len(arr)
     # Rearrange elements at each n/2, n/4, n/8, ... gaps
-    gap = n//2  
-    while gap > 0:   
-        for i in range(gap,n):             
-            temp = arr[i] 
-            j = i 
-            while  j >= gap and arr[j-gap] >temp: 
-                arr[j] = arr[j-gap] 
-                j -= gap   
-            arr[j] = temp 
+    gap = n//2
+    while gap > 0:
+        for i in range(gap,n):
+            temp = arr[i]
+            j = i
+            while  j >= gap and arr[j-gap] >temp:
+                arr[j] = arr[j-gap]
+                j -= gap
+            arr[j] = temp
         gap //= 2
 
 def main():
-    arr = [ 1,31,78,26,11,16,46] 
-    shellSort(arr) 
-    print(arr) 
+    arr = [ 1,31,78,26,11,16,46]
+    shellSort(arr)
+    print(arr)
 
 if __name__ == '__main__':
-    main() 
+    main()
