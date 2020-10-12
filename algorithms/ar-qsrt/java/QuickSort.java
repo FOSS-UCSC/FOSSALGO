52b1e393c6fe7d1e2832feed5a391e2707448d54
mertbarutcuoglu <kbarutcu@students.cs.ubc.ca>
2020-10-11 22:30:08 -0700

    Implements Quick Sort with Java. Closes issue #451

---
+++ algorithms/ar-qsrt/java/QuickSort.java
@@ -0,0 +1,58 @@
+package algorithms.ar-qsrt.java;
+
+public class QuickSort {
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        if (arr == null || arr.length == 0) return;
+
+        quicksort(0, arr.length - 1, arr);
+    }
+
+    private static void quicksort(int left, int right, int[] arr) {
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+        int pivot = arr[mid];
+
+        int partitionIndex = partition(left, right, pivot, arr);
+        quicksort(left, partitionIndex - 1, arr);
+        quicksort(partitionIndex, right, arr);
+    }
+
+    private static int partition(int left, int right, int pivot, int[] arr) {
+        while (left <= right) {
+            while (arr[left] < pivot) {
+                left++;
+            }
+            while (arr[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {15, 2, 8, 9, 0, 1, 12, 22};
+
+        System.out.print("Pre-sort: ");
+        for (int i: input) {
+            System.out.format("%d ",i);
+        }
+        
+        QuickSort.sort(input);
+
+        System.out.print("Post-sort: ");
+        for (int i: input) {
+            System.out.format("%d ",i);
+        }
+    }
+    
+}
