d11e7b98c92b2c4df70155efe3e6d1cabbd5daed
Valentina <21361479+vale-c@users.noreply.github.com>
2020-10-13 08:57:35 +0200

    Update bfs.py

--- algorithms/gr-bfsrh/python2/bfs.py
+++ algorithms/gr-bfsrh/python2/bfs.py
@@ -21,11 +21,9 @@ def bfs(graph, s):
         # If a adjacent has not been visited, then mark it 
         # visited and enqueue it
         for v in graph[current_vertex]:
-          if graph[current_vertex] == None:
-            return
-            if v not in visited:
-                visited.add(v)
-                queue.appendleft(v)
-                
+          if v not in visited:
+             visited.add(v)
+             queue.appendleft(v)
+
 # Testing Function Call
 bfs([[1,2,3],[8,6,8],[2,9,3],[2,5,7],[5,4,1],[0,8,7]], 4)
