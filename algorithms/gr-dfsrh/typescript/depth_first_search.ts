1fff46e1b08f3531192ead1e63d232a9b7179544
Famosi <simone.faggi@yahoo.it>
2020-10-13 11:31:24 +0200

    fix indentation

--- algorithms/gr-dfsrh/typescript/depth_first_search.ts
+++ algorithms/gr-dfsrh/typescript/depth_first_search.ts
@@ -13,8 +13,8 @@ class TreeNode {
      * @param {number} data
      */
     constructor(label: number, data: number){
-      this.label = label;
-      this.data = data;
+        this.label = label;
+        this.data = data;
     }
   }
   
@@ -35,11 +35,11 @@ class TreeNode {
     allowCyclic: boolean
   
     constructor(rootValue: number, allowCyclic: boolean = false) {
-      this.root = new TreeNode(0, rootValue);
-      this.adjacencies = [];
-      this.adjacencies[0] = [];
-      this.nodes = [];
-      this.allowCyclic = allowCyclic;
+        this.root = new TreeNode(0, rootValue);
+        this.adjacencies = [];
+        this.adjacencies[0] = [];
+        this.nodes = [];
+        this.allowCyclic = allowCyclic;
     }
   
     /**
@@ -48,8 +48,8 @@ class TreeNode {
      * @param {TreeNode} node
      */
     addNode(node: TreeNode){
-      this.adjacencies[node.label] = [];
-      this.nodes.push(node);
+        this.adjacencies[node.label] = [];
+        this.nodes.push(node);
     }
   
     /**
@@ -59,8 +59,8 @@ class TreeNode {
      * @param {number} node2
      */
     setConnection(node1Label: number, node2Label: number){
-      if(!this.allowCyclic && node1Label === node2Label) throw Error('Graph cannot be cyclic');
-      this.adjacencies[node1Label].push(node2Label);
+        if(!this.allowCyclic && node1Label === node2Label) throw Error('Graph cannot be cyclic');
+        this.adjacencies[node1Label].push(node2Label);
     }
   
     /**
@@ -70,30 +70,30 @@ class TreeNode {
      * @returns {array}
      */
     depthFirstSearch(cb?: Function){
-      const visitedNodes : Array<number> = [];
-      let nodesToProcess : Array<number> = [0]
-      while(nodesToProcess.length){
-        let node = nodesToProcess.shift();
-  
-        // process the node if there is a callback
-        if(cb){
-          const result = cb(node, visitedNodes, nodesToProcess);
-          if(result === true) break;
+        const visitedNodes : Array<number> = [];
+        let nodesToProcess : Array<number> = [0]
+        while(nodesToProcess.length){
+            let node = nodesToProcess.shift();
+    
+            // process the node if there is a callback
+            if(cb){
+            const result = cb(node, visitedNodes, nodesToProcess);
+            if(result === true) break;
+            }
+    
+            if(!(visitedNodes.indexOf(node) > -1)){
+            visitedNodes.push(node);
+            if(!this.adjacencies[node]) continue;
+            nodesToProcess = this.adjacencies[node].concat(nodesToProcess);
+            }
         }
-  
-        if(!(visitedNodes.indexOf(node) > -1)){
-          visitedNodes.push(node);
-          if(!this.adjacencies[node]) continue;
-          nodesToProcess = this.adjacencies[node].concat(nodesToProcess);
-        }
-      }
-      
-      return visitedNodes;
+        
+        return visitedNodes;
     }
-  }
+}
   
-  function main () {
-	const tree = new Tree(100);
+function main () {
+    const tree = new Tree(100);
     const node1 = new TreeNode(1, 99);
     const node2 = new TreeNode(2, 3);
     const node3 = new TreeNode(3, 19);
@@ -121,7 +121,7 @@ class TreeNode {
     tree.setConnection(3, 7);
     tree.setConnection(4, 8);
     tree.setConnection(4, 9);
-    
+
     console.log("Visited Nodes DFS:\n", JSON.stringify(tree.depthFirstSearch()));
 }
 
