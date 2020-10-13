c87bdb3f4db61012717982f12d780a6599b2b0ce
Famosi <simone.faggi@yahoo.it>
2020-10-13 14:16:16 +0200

    fix semicolon

--- algorithms/gr-dfsrh/typescript/depth_first_search.ts
+++ algorithms/gr-dfsrh/typescript/depth_first_search.ts
@@ -35,11 +35,11 @@ class TreeNode {
     allowCyclic: boolean
   
     constructor(rootValue: number, allowCyclic: boolean = false) {
-        this.root = new TreeNode(0, rootValue);
-        this.adjacencies = [];
-        this.adjacencies[0] = [];
-        this.nodes = [];
-        this.allowCyclic = allowCyclic;
+      this.root = new TreeNode(0, rootValue);
+      this.adjacencies = [];
+      this.adjacencies[0] = [];
+      this.nodes = [];
+      this.allowCyclic = allowCyclic;
     }
   
     /**
@@ -48,8 +48,8 @@ class TreeNode {
      * @param {TreeNode} node
      */
     addNode(node: TreeNode){
-        this.adjacencies[node.label] = [];
-        this.nodes.push(node);
+      this.adjacencies[node.label] = [];
+      this.nodes.push(node);
     }
   
     /**
@@ -59,8 +59,8 @@ class TreeNode {
      * @param {number} node2
      */
     setConnection(node1Label: number, node2Label: number){
-        if(!this.allowCyclic && node1Label === node2Label) {throw Error("Graph cannot be cyclic")};
-        this.adjacencies[node1Label].push(node2Label);
+      if(!this.allowCyclic && node1Label === node2Label) {throw Error("Graph cannot be cyclic")};
+      this.adjacencies[node1Label].push(node2Label);
     }
   
     /**
@@ -70,59 +70,57 @@ class TreeNode {
      * @returns {array}
      */
     depthFirstSearch(cb?: Function){
-        const visitedNodes : Array<number> = [];
-        let nodesToProcess : Array<number> = [0]
-        while(nodesToProcess.length){
-            let node = nodesToProcess.shift();
-    
-            // process the node if there is a callback
-            if(cb){
-            const result = cb(node, visitedNodes, nodesToProcess);
-            if(result === true) {break};
-            }
-    
-            if(!(visitedNodes.indexOf(node) > -1)){
-            visitedNodes.push(node);
-            if(!this.adjacencies[node]) {continue};
-            nodesToProcess = this.adjacencies[node].concat(nodesToProcess);
-            }
+      const visitedNodes : Array<number> = [];
+      let nodesToProcess : Array<number> = [0]
+      while(nodesToProcess.length){
+        let node = nodesToProcess.shift();
+
+        // process the node if there is a callback
+        if(cb){
+          const result = cb(node, visitedNodes, nodesToProcess);
+          if(result === true) {break};
         }
-        
-        return visitedNodes;
+
+        if(!(visitedNodes.indexOf(node) > -1)){
+          visitedNodes.push(node);
+          if(!this.adjacencies[node]) {continue};
+          nodesToProcess = this.adjacencies[node].concat(nodesToProcess);
+        }
+      }
+      
+      return visitedNodes;
     }
 }
   
 function main () {
-    const tree = new Tree(100);
-    const node1 = new TreeNode(1, 99);
-    const node2 = new TreeNode(2, 3);
-    const node3 = new TreeNode(3, 19);
-    const node4 = new TreeNode(4, -20);
-    const node5 = new TreeNode(5, 33);
-    const node6 = new TreeNode(6, 3663);
-    const node7 = new TreeNode(7, 3663);
-    const node8 = new TreeNode(8, 3663);
-    const node9 = new TreeNode(9, 3663);
-    tree.addNode(node1);
-    tree.addNode(node2);
-    tree.addNode(node3);
-    tree.addNode(node4);
-    tree.addNode(node5);
-    tree.addNode(node6);
-    tree.addNode(node7);
-    tree.addNode(node8);
-    tree.addNode(node9);
-    tree.setConnection(0, 1);
-    tree.setConnection(0, 2);
-    tree.setConnection(2, 6);
-    tree.setConnection(1, 3);
-    tree.setConnection(1, 4);
-    tree.setConnection(1, 5);
-    tree.setConnection(3, 7);
-    tree.setConnection(4, 8);
-    tree.setConnection(4, 9);
-
-    console.log("Visited Nodes DFS:\n", JSON.stringify(tree.depthFirstSearch()));
+  const tree = new Tree(100);
+  const node1 = new TreeNode(1, 99);
+  const node2 = new TreeNode(2, 3);
+  const node3 = new TreeNode(3, 19);
+  const node4 = new TreeNode(4, -20);
+  const node5 = new TreeNode(5, 33);
+  const node6 = new TreeNode(6, 3663);
+  const node7 = new TreeNode(7, 3663);
+  const node8 = new TreeNode(8, 3663);
+  const node9 = new TreeNode(9, 3663);
+  tree.addNode(node1);
+  tree.addNode(node2);
+  tree.addNode(node3);
+  tree.addNode(node4);
+  tree.addNode(node5);
+  tree.addNode(node6);
+  tree.addNode(node7);
+  tree.addNode(node8);
+  tree.addNode(node9);
+  tree.setConnection(0, 1);
+  tree.setConnection(0, 2);
+  tree.setConnection(2, 6);
+  tree.setConnection(1, 3);
+  tree.setConnection(1, 4);
+  tree.setConnection(1, 5);
+  tree.setConnection(3, 7);
+  tree.setConnection(4, 8);
+  tree.setConnection(4, 9);
 }
 
 main()
