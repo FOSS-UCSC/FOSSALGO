8155a07781d01885f10b499d6ebaa248ad335dc5
iamyajat <myajat21@gmail.com>
2020-10-14 08:16:34 +0530

    Added Prime Number in Python3

--- algorithms/ar-prfac/python3/prime_factor.py
+++ algorithms/ar-prfac/python3/prime_factor.py
@@ -1,24 +1,16 @@
 def primefactor(arr):
     if arr<=0:
         return
-    
     while arr%2==0:
         print(2)
         arr/=2
-
     for i in range(3, int(arr*arr)+1,2):
-
         while arr%i==0:
             print(i)
             arr/=i
-
     if arr>2:
         print(int(arr))
-
-
 def main():
     primefactor(140)
-
-    
 if __name__ == '__main__':
     main()
