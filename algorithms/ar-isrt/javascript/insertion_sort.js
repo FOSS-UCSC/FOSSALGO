3b97a80e2fc1233d6f4b8aba70cd61f276fc98cb
Nimesha Dilini <37245819+EANimesha@users.noreply.github.com>
2020-10-14 12:50:52 +0530

    Updated for review comments

--- algorithms/ar-isrt/javascript/insertion_sort.js
+++ algorithms/ar-isrt/javascript/insertion_sort.js
@@ -1,20 +1,19 @@
 let insertionSort = (array) => {
-  for (outer = 1; outer < array.length; outer++) {
-    for (inner = 0; inner < outer; inner++) {
-      console.log(array.join(' '))
+  for (let outer = 1; outer < array.length; outer++) {
+    for (let inner = 0; inner < outer; inner++) {
       if (array[outer] < array[inner]) {
-        const [element] = array.splice(outer, 1)
-        array.splice(inner, 0, element)
+        const [element] = array.splice(outer, 1);
+        array.splice(inner, 0, element);
       }
     }
   }
-  return array
-}
+  return array;
+};
 
 let main = () => {
-	let arr = [9, 8, 7, 6, 5, 4 ,3 , 2, 1]
+	let arr = [9, 8, 7, 6, 5, 4 ,3 , 2, 1];
 	insertionSort(arr);
 	return arr;
 };
 
-main()
+main();
