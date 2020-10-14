762bee48e963f1cbb0bde2dfa364574a9b9dfba6
Any0ne22 <tom.gouville@protonmail.com>
2020-10-14 09:42:02 +0200

    Reducing cyclomatic complexity

--- algorithms/ar-msrt/javascript/merge_sort.js
+++ algorithms/ar-msrt/javascript/merge_sort.js
@@ -1,28 +1,14 @@
 function merge(array1, array2) {
-    if (array1 === []) {
-        return array2;
-    }
-    else if (array2 === []) {
-        return array1;
-    }
-    else {
-        let output = [];
-        while (array1.length > 0 || array2.length > 0) {
-            if (array1.length === 0) {
-                output.push(array2.shift());
-            }
-            else if (array2.length === 0) {
-                output.push(array1.shift());
-            }
-            else if (array1[0] < array2[0]) {
-                output.push(array1.shift());
-            }
-            else {
-                output.push(array2.shift());
-            }
+    let output = [];
+    while (array1.length > 0 || array2.length > 0) {
+        if (array2.length === 0 || array1[0] < array2[0]) {
+            output.push(array1.shift());
         }
-        return output;
+        else {
+            output.push(array2.shift());
+        }
     }
+    return output;
 }
 function mergeSort(array) {
     const n = array.length;
