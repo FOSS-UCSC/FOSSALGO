f05f2b75d049c0f3c796a409668c9586fb5a13c3
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-10-14 22:09:08 +0530

    #556 #558

--- data-structures/ds-pque/java/PriorityQueue.java
+++ data-structures/ds-pque/java/PriorityQueue.java
@@ -1,28 +1,78 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
 
-public class Main {
+public class PriorityQueueUsingHeap {
+    public static class PriorityQueue {
+        ArrayList<Integer> data;
 
-    public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int[] ranks = {22,99,11,88,3,4,1};
-
-      //Adding Values
-        for(int val : ranks){
-            pq.add(val);
+        public PriorityQueue() {
+            data = new ArrayList<>();
         }
 
-        //Showing the peek values
-        while (pq.size()>0){
-            System.out.print(pq.peek()+" ");
-            
-            //Removing Values
-            pq.remove();
+        //Add Element In PriorityQueue
+        public void add(int val) {
+            data.add(val);
+            upheapify(data.size() - 1);
         }
-        System.out.println();
-        
-        //For reversing the Priority Queue
-        //PriorityQueue<Integer> pqReverse = new PriorityQueue<>(Collections.reverseOrder());
-        
+
+        private void upheapify(int i) {
+            if (i == 0) {
+                return;
+            }
+
+            int pi = (i - 1) / 2;
+            if (data.get(i) < data.get(pi)) {
+                swap(i, pi);
+                upheapify(pi);
+            }
+        }
+
+        private void swap(int i, int j) {
+            int ith = data.get(i);
+            int jth = data.get(j);
+            data.set(i, jth);
+            data.set(j, ith);
+        }
+
+
+        //Remove Element In PriorityQueue
+        public int remove() {
+            if (this.size() == 0) {
+                System.out.println("Underflow");
+                return -1;
+            }
+            swap(0, data.size() - 1);
+            int val = data.remove(data.size() - 1);
+            downheapify(0);
+            return val;
+        }
+
+        private void downheapify(int pi) {
+            int min = pi;
+            int li = 2 * pi + 1;
+            if (li < data.size() && data.get(li) < data.get(min)) {
+                min = li;
+            }
+            int ri = 2 * pi + 2;
+            if (ri < data.size() && data.get(ri) < data.get(min)) {
+                min = ri;
+            }
+            if (min != pi) {
+                swap(pi, min);
+                downheapify(min);
+            }
+
+        }
+
+        public int peek() {
+            if (this.size() == 0) {
+                System.out.println("Underflow");
+                return -1;
+            }
+            return data.get(0);
+        }
+
+        public int size() {
+            return data.size();
+        }
     }
 }
