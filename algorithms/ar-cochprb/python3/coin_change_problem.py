dcda2deae9ff9aeefbfa98e10adad110e67a8b87
Sg04 <58985778+sg-4@users.noreply.github.com>
2020-10-15 19:43:55 +0530

    added coin change problem in python 3 (#581)
    * added coin change problem in python 3
    
    * changes made ro coin change problem
    
    * Readme updated
    
    Co-authored-by: s <shreyagupta4700@gmail.com>
    Co-authored-by: Sachintha <ravindusachintha53@gmail.com>

---
+++ algorithms/ar-cochprb/python3/coin_change_problem.py
@@ -0,0 +1,16 @@
+#python3 program for coin change problem using dynamic programming approach
+def coin_change(S, m, n):
+    solution = [0 for k in range(n+1)]
+    solution[0] = 1
+
+    for i in range(0,m):
+        for j in range(S[i],n+1):
+              solution[j] = solution[j] + solution[j-S[i]]
+
+    return solution[n]
+
+#driver code
+S = [1, 2, 3]
+m = len(S)
+n = 4
+print(coin_change(S, m, n))
