23b6b68f51198fc9f137c51b9c93234a3b7cf0cf
Uddeshya Tyagi <uddeshyatyagi775@gmail.com>
2020-10-15 22:22:01 +0530

    Reduced the scope of variables

--- algorithms/ar-radsrt/cpp/radix_sort.cpp
+++ algorithms/ar-radsrt/cpp/radix_sort.cpp
@@ -8,18 +8,18 @@ void display(int *array, int size) {
    cout << endl;
 }
 void radixSort(int *arr, int n, int max) {
-   int i, j, m, p = 1, index, temp, count = 0;
+
    list<int> pocket[10];      //radix of decimal number is 10
-   for(i = 0; i< max; i++) {
-      m = pow(10, i+1);
-      p = pow(10, i);
-      for(j = 0; j<n; j++) {
-         temp = arr[j]%m;
-         index = temp/p;      //find index for pocket array
+   for(int i = 0; i< max; i++) {
+      int m = pow(10, i+1);
+      int p = pow(10, i);
+      for(int j = 0; j<n; j++) {
+         int temp = arr[j]%m;
+         int index = temp/p;      //find index for pocket array
          pocket[index].push_back(arr[j]);
       }
-      count = 0;
-      for(j = 0; j<10; j++) {
+      int count = 0;
+      for(int j = 0; j<10; j++) {
          //delete from linked lists and store to array
          while(!pocket[j].empty()) {
             arr[count] = *(pocket[j].begin());
