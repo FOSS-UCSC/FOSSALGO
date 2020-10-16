e7c81f22686d25a454347913261bebd3ca47b296
Rishie Ramasinghe <rsnramasinghe@gmail.com>
2020-10-16 16:44:37 +0000

    Add dijkstra algo

---
+++ algorithms/gr-dij/python3/dijkstra.py
@@ -0,0 +1,83 @@
+# Library for INT_MAX
+import sys
+
+
+class Dijkstra:
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def print_solution(self, dist):
+        print("Vertex Distance from Source")
+        for node in range(self.V):
+            print(node, "tt", dist[node])
+
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minDistance(self, dist, split_set):
+
+        # Initialize minimum distance for next node
+        min = sys.maxsize
+
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for v in range(self.V):
+            if dist[v] < min and split_set[v] is False:
+                min = dist[v]
+                min_index = v
+
+        return min_index
+
+    # Function that implements Dijkstra's single source
+    # shortest path algorithm for a graph represented
+    # using adjacency matrix representation
+    def dijkstra(self, src):
+
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        split_set = [False] * self.V
+
+        for count in range(self.V):
+
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # u is always equal to src in first iteration
+            u = self.minDistance(dist, split_set)
+
+            # Put the minimum distance vertex in the
+            # shortest path tree
+            split_set[u] = True
+
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and \
+                        split_set[v] is False and \
+                        dist[v] > dist[u] + self.graph[u][v]:
+                    dist[v] = dist[u] + self.graph[u][v]
+
+        self.print_solution(dist)
+
+
+def main():
+    g = Dijkstra(9)
+    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+               [4, 0, 8, 0, 0, 0, 0, 11, 0],
+               [0, 8, 0, 7, 0, 4, 0, 0, 2],
+               [0, 0, 7, 0, 9, 14, 0, 0, 0],
+               [0, 0, 0, 9, 0, 10, 0, 0, 0],
+               [0, 0, 4, 14, 10, 0, 2, 0, 0],
+               [0, 0, 0, 0, 0, 2, 0, 1, 6],
+               [8, 11, 0, 0, 0, 0, 1, 0, 7],
+               [0, 0, 2, 0, 0, 0, 6, 7, 0]
+               ]
+
+    g.dijkstra(0)
+
+if __name__ == '__main__':
+    main()
