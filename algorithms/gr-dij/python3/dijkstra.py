dcbfa8954bc2e83cb34686eb49a24c45afc8c349
Rishie Ramasinghe <rsnramasinghe@gmail.com>
2020-10-16 19:57:38 +0000

    fixed error

--- algorithms/gr-dij/python3/dijkstra.py
+++ algorithms/gr-dij/python3/dijkstra.py
@@ -1,83 +1,78 @@
 # Library for INT_MAX
 import sys
 
+class Graph():
 
-class Dijkstra:
+	def __init__(self, vertices):
+		self.V = vertices
+		self.graph = [[0 for column in range(vertices)]
+					for row in range(vertices)]
 
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
+	def printSolution(self, dist):
+		print ("Vertex tDistance from Source")
+		for node in range(self.V):
+			print (node, "t", dist[node])
 
-    def print_solution(self, dist):
-        print("Vertex Distance from Source")
-        for node in range(self.V):
-            print(node, "tt", dist[node])
+	# A utility function to find the vertex with
+	# minimum distance value, from the set of vertices
+	# not yet included in shortest path tree
+	def minDistance(self, dist, sptSet):
 
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
-    def minDistance(self, dist, split_set):
+		# Initilaize minimum distance for next node
+		min1 = sys.maxsize
 
-        # Initialize minimum distance for next node
-        min = sys.maxsize
+		# Search not nearest vertex not in the
+		# shortest path tree
+		for v in range(self.V):
+			if dist[v] < min1 and sptSet[v] == False:
+				min1 = dist[v]
+				min_index = v
 
-        # Search not nearest vertex not in the
-        # shortest path tree
-        for v in range(self.V):
-            if dist[v] < min and split_set[v] is False:
-                min = dist[v]
-                min_index = v
+		return min_index
 
-        return min_index
+	# Funtion that implements Dijkstra's single source
+	# shortest path algorithm for a graph represented
+	# using adjacency matrix representation
+	def dijkstra(self, src):
 
-    # Function that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
-    def dijkstra(self, src):
+		dist = [sys.maxsize] * self.V
+		dist[src] = 0
+		sptSet = [False] * self.V
 
-        dist = [sys.maxsize] * self.V
-        dist[src] = 0
-        split_set = [False] * self.V
+		for cout in range(self.V):
 
-        for count in range(self.V):
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # u is always equal to src in first iteration
-            u = self.minDistance(dist, split_set)
+			# Pick the minimum distance vertex from
+			# the set of vertices not yet processed.
+			# u is always equal to src in first iteration
+			u = self.minDistance(dist, sptSet)
 
-            # Put the minimum distance vertex in the
-            # shortest path tree
-            split_set[u] = True
+			# Put the minimum distance vertex in the
+			# shotest path tree
+			sptSet[u] = True
 
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
-            for v in range(self.V):
-                if self.graph[u][v] > 0 and \
-                        split_set[v] is False and \
-                        dist[v] > dist[u] + self.graph[u][v]:
-                    dist[v] = dist[u] + self.graph[u][v]
+			# Update dist value of the adjacent vertices
+			# of the picked vertex only if the current
+			# distance is greater than new distance and
+			# the vertex in not in the shotest path tree
+			for v in range(self.V):
+				if self.graph[u][v] > 0 and \
+					sptSet[v] == False and \
+					dist[v] > dist[u] + self.graph[u][v]:
+					dist[v] = dist[u] + self.graph[u][v]
 
-        self.print_solution(dist)
+		self.printSolution(dist)
 
+# Driver program
+g = Graph(9)
+g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+		[4, 0, 8, 0, 0, 0, 0, 11, 0],
+		[0, 8, 0, 7, 0, 4, 0, 0, 2],
+		[0, 0, 7, 0, 9, 14, 0, 0, 0],
+		[0, 0, 0, 9, 0, 10, 0, 0, 0],
+		[0, 0, 4, 14, 10, 0, 2, 0, 0],
+		[0, 0, 0, 0, 0, 2, 0, 1, 6],
+		[8, 11, 0, 0, 0, 0, 1, 0, 7],
+		[0, 0, 2, 0, 0, 0, 6, 7, 0]
+		];
 
-def main():
-    g = Dijkstra(9)
-    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
-               [4, 0, 8, 0, 0, 0, 0, 11, 0],
-               [0, 8, 0, 7, 0, 4, 0, 0, 2],
-               [0, 0, 7, 0, 9, 14, 0, 0, 0],
-               [0, 0, 0, 9, 0, 10, 0, 0, 0],
-               [0, 0, 4, 14, 10, 0, 2, 0, 0],
-               [0, 0, 0, 0, 0, 2, 0, 1, 6],
-               [8, 11, 0, 0, 0, 0, 1, 0, 7],
-               [0, 0, 2, 0, 0, 0, 6, 7, 0]
-               ]
-
-    g.dijkstra(0)
-
-
-if __name__ == '__main__':
-    main()
+g.dijkstra(0);
