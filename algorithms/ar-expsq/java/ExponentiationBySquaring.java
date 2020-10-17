5e8f53a135fdfe527e75ac7d603f6c2707b856ce
Rishie Ramasinghe <rsnramasinghe@gmail.com>
2020-10-17 19:38:07 +0000

    Added Exponentiation By Squaring

---
+++ algorithms/ar-expsq/java/ExponentiationBySquaring.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+public class ExponentiationBySquaring {
+    static long N = 1000000007L; // prime modulo value
+    public static void main(String[] args)
+    {
+        long base = 5;
+        long exp = 100000;
+
+        long modulo = exponentiation(base, exp);
+        System.out.println(modulo);
+    }
+
+    static long exponentiation(long base, long exp)
+    {
+        if (exp == 0)
+            return 1;
+
+        if (exp == 1)
+            return base % N;
+
+        long t = exponentiation(base, exp / 2);
+        t = (t * t) % N;
+
+        // if exponent is even value
+        if (exp % 2 == 0)
+            return t;
+
+            // if exponent is odd value
+        else
+            return ((base % N) * t) % N;
+    }
+}
