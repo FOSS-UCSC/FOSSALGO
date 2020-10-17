4167222ce52f3d026fbf3ac858aa1981c1f94ff7
hasalfernando <witty.leo98@gmail.com>
2020-10-17 17:15:48 +0530

    Further removed trailing white space issues

--- algorithms/ar-hsrt/python3/heap_sort.py
+++ algorithms/ar-hsrt/python3/heap_sort.py
@@ -1,15 +1,15 @@
-def heapify(arr, n, i): 
+def heapify(arr, n, i):
     largest = i
     l = 2 * i + 1
     r = 2 * i + 2
 
     if l < n and arr[i] < arr[l]:
-        largest = l
-
+		largest = l
+	
     if r < n and arr[largest] < arr[r]:
-        largest = r
+		largest = r
 
-    if largest != i: 
+    if largest != i:
         arr[i],arr[largest] = arr[largest],arr[i]
         heapify(arr, n, largest)
   
