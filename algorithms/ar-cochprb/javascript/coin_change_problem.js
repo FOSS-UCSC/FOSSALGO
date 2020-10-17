9068390b50ede4bf50e73a140d51e6227e92d132
Ishan Avishka <ishanavishka14@gmail.com>
2020-10-17 19:00:25 +0530

    Coin Change Problem in Javascript #571

--- algorithms/ar-cochprb/javascript/coin_change_problem.js
+++ algorithms/ar-cochprb/javascript/coin_change_problem.js
@@ -1,14 +1,19 @@
 
 function waysofChange(s, numOfCoins, n) {
     
-    if(n === 0) return 1; 
+    if(n === 0) {
+        return 1; 
+    }
     
-    if(n < 0) return 0; 
+    if(n < 0){
+        return 0; 
+    }
     
-    if(numOfCoins < 0 && n > 0) return 0; 
+    if(numOfCoins < 0 && n > 0){
+        return 0; 
+    }
 
-    return waysofChange(s, numOfCoins, n - s[numOfCoins]) + 
-    waysofChange(s, numOfCoins - 1, n);
+    return waysofChange(s, numOfCoins, n - s[numOfCoins]) + waysofChange(s, numOfCoins - 1, n);
 }
 
 var s = [1, 2, 3];
