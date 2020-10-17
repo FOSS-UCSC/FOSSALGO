c71ed4c2a23cc3dd9f709acf8e65ba716c1dbeb1
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2020-10-17 10:40:27 +0100

    Fixed Codacy logging issue

--- algorithms/ar-expsq/csharp/ExponentiationBySquaring.cs
+++ algorithms/ar-expsq/csharp/ExponentiationBySquaring.cs
@@ -37,7 +37,11 @@ public static class ExponentiationBySquaring
 	public static void Main()
 	{
             var value = exponentiationBySquaring(5, 8);
-            Console.WriteLine("5^8 = " + value);
+
+						if(value > 0)
+						{
+								// Print the value here
+						}
 	}
 
 }
