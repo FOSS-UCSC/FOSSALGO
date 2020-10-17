f7c285c6631f1f1d690faf2d008f01cfd8d0fe9b
danielromeo <danielromeo99@gmail.com>
2020-10-17 16:03:50 +0200

    [update] fixed jsLint/tsLint errors, added inserion sort Typescript algorithm

--- algorithms/ar-isrt/typescript/insertion_sort.ts
+++ algorithms/ar-isrt/typescript/insertion_sort.ts
@@ -1,31 +1,27 @@
-const swap = (arr: Array<number>, a: number, b: number)=>{
+const swap = (arr: Array<number>, a: number, b: number) =>{
 	let temp = arr[a];
 	arr[a] = arr[b];
 	arr[b] = temp;
-}
-
-const insertionsort = (values: Array<number>)=>{
-
+};
+const insertionsort = (values: Array<number>) =>{
 	for (let i = 1; i < values.length; i++) 
-    { 
+    {
         let j = i; 
-        
         while (j > 0 && values[j] <  values[j -1]) 
-        { 
-           	 swap(values, j, j - 1);
-        	
+        {
+            swap(values, j, j - 1);
         	j--;
         }    
-    } 
+    }
 	return values;
 }
-
-
 function main () {
 	let input: number[] = [2, 3, 0, 4];
 	let output: number[] = insertionsort(input);
 	for (let x in output) {
-        console.log(output[x]);
+        if (output.hasOwnProperty(x)) {
+            console.log(output[x]);
+        }
 	}
 }
 
