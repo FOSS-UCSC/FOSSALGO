ec841eddd671c2f5819b95b6b073d2a84b0b801b
devblin <110119035@nitt.edu>
2020-10-18 16:28:57 +0530

    Added exponentiation by squaring algorithm in C

---
+++ algorithms/ar-expsq/c/exponentiation_by_squaring.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void exponentiationSquaring(long base, long power) {
+	long result = 1;
+
+	while(power > 0) { //run loop till power is greater than zero
+		if(power % 2 != 0) { //if power is ODD multiply result by base
+			result = result * base;
+		}
+		base = base * base; //squaring base
+		power /= 2; //reduce power to half
+	}
+	
+	printf("%lu\n", result);
+}
+
+int main() {
+	long base = 3;
+	long power = 2;
+
+	exponentiationSquaring(base, power);
+	return 0;
+}
