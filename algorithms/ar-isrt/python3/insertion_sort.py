8e20ae7ba860544de8a2140ee46aa1ce1a17c9cf
Liam Major <61054048+liamm91@users.noreply.github.com>
2020-10-18 12:07:06 -0400

    Added Insertion Sort Python 3 (#663)
    * added my solution to insertion sort
    
    * Added Insertion Sort in Python3 to the README.md
    
    Added Insertion Sort in Python3 to the README.md
    
    Co-authored-by: liamm91 <liamm91@kprschools.ca>
    Co-authored-by: Sachintha <ravindusachintha53@gmail.com>
    Co-authored-by: Danushka Herath <Danushkaherath96@gmail.com>

--- algorithms/ar-isrt/python3/insertion_sort.py
+++ algorithms/ar-isrt/python3/insertion_sort.py
@@ -1,15 +1,22 @@
 def insertion_sort(array):
-    n = len(array)
-    for i in range(1, n):
-        temp = array[i]
-        print(temp)
-        j = i
-        while j > 0 and temp < array[j - 1]:
-            array[j] = array[j - 1]
-            j -= 1
-        array[j] = temp
-    return array
+    if not array or len(array) == 1: return array # the inputted array has nothing inside or has one element
+
+    sorted_list = []
+    for item in array: # looping through the main array
+        # iterating through the list and comparing each item
+        for i in sorted_list: # comparing every item in the sorted_list
+            if item < i:
+                sorted_list.insert(sorted_list.index(i), item) # inserting item into sorted_list because it is less than the item
+                break
+        else:
+            sorted_list.append(item) # nothing was inserted, therefore, item is greather than anything in sorted_list
+
+    return sorted_list
 
 
-a = [5, 2, 4, 3, 0, 6, 7, 8, 11]
-print(insertion_sort(a))
+def main():
+    a = [5, 2, 4, 3, 0, 6, 7, 8, 11]
+    print(insertion_sort(a))
+
+if __name__ == "__main__":
+    main()
