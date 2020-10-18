b86145fb0db28ed40ffddf50322584526ebe51c5
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-10-18 20:12:55 +0200

    fixed all codacy errors

--- algorithms/ar-isrt/typescript/insertion_sort.ts
+++ algorithms/ar-isrt/typescript/insertion_sort.ts
@@ -1,28 +1,24 @@
-const swap = (arr: Array<number>, a: number, b: number) =>{
+const swap = (arr: Array<number>, a: number, b: number) => {
 	let temp = arr[a];
 	arr[a] = arr[b];
 	arr[b] = temp;
 };
-const insertionsort = (values: Array<number>) =>{
-	for (let i = 1; i < values.length; i++) 
+const insertionsort = (values: Array<number>) => {
+	for (let i = 1; i < values.length; i++)
     {
-        let j = i; 
+        let j = i;
         while (j > 0 && values[j] <  values[j -1]) 
         {
             swap(values, j, j - 1);
-        	j--;
+            j--;
         }    
     }
 	return values;
-}
+};
 function main () {
 	let input: number[] = [2, 3, 0, 4];
 	let output: number[] = insertionsort(input);
-	for (let x in output) {
-        if (output.hasOwnProperty(x)) {
-            console.log(output[x]);
-        }
-	}
+	return output;
 }
 
 main();
