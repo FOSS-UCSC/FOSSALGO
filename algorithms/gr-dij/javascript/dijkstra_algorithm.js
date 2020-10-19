c185e1ba09a5f50d0038c0db54dcb138a22c5f0e
LaxmiNyamagoud <60287838+LaxmiNyamagoud@users.noreply.github.com>
2020-10-19 12:16:22 +0530

    Update dijkstra_algorithm.js
    I have made a changes in the program

--- algorithms/gr-dij/javascript/dijkstra_algorithm.js
+++ algorithms/gr-dij/javascript/dijkstra_algorithm.js
@@ -1,52 +1,74 @@
-/*
-* Dijkstra's single source shortest path algorithm in JavaScript.
- */
- const dijkstraAlgorithm = function (graph, start) {
 
-    //the distances from the start node to all other nodes
-    var distance = [];
-    //Initial distance is "Infinity"
-    for (var i = 0; i < graph.length; i++) distance[i] = Number.MAX_VALUE;
-    //The distance from the start node to itself is 0
-    distance[start] = 0;
-
-    //it contains whether a node was already visited
-    var visited = [];
-
-    //While there are nodes left to visit
-    while (true) {
-        //find the node with the currently shortest distance from the start node
-        var shortestDistance = Number.MAX_VALUE;
-        var shortestIndex = -1;
-        for (var i = 0; i < graph.length; i++) {
-            if (distance[i] < shortestDistance && !visited[i]) {
-                shortestDistance = distance[i];
-                shortestIndex = i;
-            }
+//created question as object
+const question = {
+    start: {A: 5, D: 8},
+    A: {B: 9, C: 3},
+    D: {C: 4, E: 6},
+    C: {B: 5, E: 2},
+    B: {end: 7},
+    E: {end: 4},
+    end: {}
+  };
+  
+  const shortestCostNode = (costs, processed) => {
+    return Object.keys(costs).reduce((lowest, node) => {
+      if (lowest === null || costs[node] < costs[lowest]) {
+        if (!processed.includes(node)) {
+          lowest = node;
         }
-
-        console.log("Visiting node " + shortestDistance + " with current distance " + shortestDistance);
-
-        if (shortestIndex === -1) {
-            // There was no node not yet visited
-            return distance;
-        }
-
-        //for all neighboring nodes....
-        for (var i = 0; i < graph[shortestIndex].length; i++) {
-            //...if the path over this edge is shorter...
-            if (graph[shortestIndex][i] !== 0 && distance[i] > distance[shortestIndex] + graph[shortestIndex][i]) {
-                //...Save this path as new shortest path.
-                distance[i] = distance[shortestIndex] + graph[shortestIndex][i];
-                console.log("Updating distance of node " + i + " to " + distance[i]);
-            }
-        }
-        // Lastly we finished with this node.
-        visited[shortestIndex] = true;
-        console.log("Visited nodes: " + visited);
-        console.log("Currently lowest distance: " + distance);
-
+      }
+      return lowest;
+    }, null);
+  };
+  
+  // this function returns the minimum cost and path to reach end 
+  const shortestPath = (graph) => {
+  
+    // track lowest cost to reach each node
+    const costs = Object.assign({end: Infinity}, graph.start);
+  
+    const parents = {end: null};
+    for (let child in graph.start) {
+      parents[child] = 'start';
     }
-};
-
-module.exports = {dijkstraAlgorithm};
+  
+    // track nodes that have already been processed
+    const processed = [];
+  
+    let node = shortestCostNode(costs, processed);
+  
+    while (node) {
+      let cost = costs[node];
+      let children = graph[node];
+      for (let n in children) {
+        let newCost = cost + children[n];
+        if (!costs[n]) {
+          costs[n] = newCost;
+          parents[n] = node;
+        }
+        if (costs[n] > newCost) {
+          costs[n] = newCost;
+          parents[n] = node;
+        }
+      }
+      processed.push(node);
+      node = shortestCostNode(costs, processed);
+    }
+  
+    let optimalPath = ['end'];
+    let parent = parents.end;
+    while (parent) {
+      optimalPath.push(parent);
+      parent = parents[parent];
+    }
+    optimalPath.reverse();
+  
+    const result = {
+      distance: costs.end,
+      path: optimalPath
+    };
+  
+    return result;
+  };
+  
+  console.log(shortestPath(question));
