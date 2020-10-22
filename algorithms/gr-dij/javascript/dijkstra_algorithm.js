1bfffbcc81eeb91f06dfb99e68959a98c5fddabe
LaxmiNyamagoud <60287838+LaxmiNyamagoud@users.noreply.github.com>
2020-10-22 13:13:41 +0530

    dijkstra_algorithm.js

--- algorithms/gr-dij/javascript/dijkstra_algorithm.js
+++ algorithms/gr-dij/javascript/dijkstra_algorithm.js
@@ -1,12 +1,12 @@
 
 //created question as object
-const graph = {
-    start: {A: 5, D: 8},
-    A: {B: 9, C: 3},
-    D: {C: 4, E: 6},
-    C: {B: 5, E: 2},
-    B: {end: 7},
-    E: {end: 4},
+  const graph = {
+    start: { A: 5, D: 8 },
+    A: { B: 9, C: 3 },
+    D: { C: 4, E: 6 },
+    C: { B: 5, E: 2 },
+    B: { en d:  },
+    E: { end: 4 },
     end: {}
   };
 
@@ -26,7 +26,12 @@ const graph = {
 
     // track lowest cost to reach each node
     const costs = Object.assign({end: Infinity}, graph.start);
-    const parents = { end: null, A: "start", D: "start"};
+    const parents = {end: null};
+    for (let child in graph.start) {
+      if((graph.start).hasOwnProperty(child)){
+        parents[child] = 'start';
+      }
+    }
 
     // track nodes that have already been processed
     const processed = [];
@@ -35,26 +40,23 @@ const graph = {
     while (node) {
       let cost = costs[node];
       let children = graph[node];
-      for (let n in children) {
-        if(children.hasOwnProperty(n)){
-          let newCost = cost + children[n];
-          if (!costs[n] || costs[n] > newCost) {
-          costs[n] = newCost;
-          parents[n] = node;
-        }
+      for (const [key, child] of Object.entries(children)) {
+      let newCost = cost + children[key];
+        if (!costs[key] || costs[key] > newCost) {
+          costs[key] = newCost;
+          parents[key] = node;
         }
       }
       processed.push(node);
       node = shortestCostNode(costs, processed);
     }
 
-    let optimalPath = ["end"];
+    const optimalPath = ["end"];
     let parent = parents.end;
     while (parent) {
-      optimalPath.push(parent);
+      optimalPath.unshift(parent);
       parent = parents[parent];
     }
-    optimalPath.reverse();
 
     const result = {
       distance: costs.end,
