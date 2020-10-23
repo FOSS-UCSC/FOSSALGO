e1af96b540c7d395fb99858bd74615ef0a52f15a
David Azevedo <davidazevedo1996@hotmail.com>
2020-10-23 12:21:59 +0100

    Fix Codacy Errors

--- algorithms/ar-lreg/javascript/linear_regression.js
+++ algorithms/ar-lreg/javascript/linear_regression.js
@@ -1,29 +1,29 @@
 // Using the Least Squares Method
 function linearRegression (points) {
   const n = points.length;
-  const x_points = points.map(([x, _]) => x);
-  const y_points = points.map(([_, y]) => y);
+  const xPoints = points.map(([x, _]) => x);
+  const yPoints = points.map(([_, y]) => y);
 
   // Calculate the average of each axis for the input points
-  const x_average = x_points.reduce((x_0, x_1) => x_0 + x_1, 0)/n;
-  const y_average = y_points.reduce((y_0, y_1) => y_0 + y_1, 0)/n;
+  const xAverage = xPoints.reduce((x0, x1) => x0 + x1, 0)/n;
+  const yAverage = yPoints.reduce((y0, y1) => y0 + y1, 0)/n;
 
   // Calculate cross products
-  const cross_product_xy = crossProduct(y_points, x_points) - n * y_average * x_average;
-  const cross_product_xx = crossProduct(x_points, x_points) - n * x_average * x_average;
+  const crossProductXY = crossProduct(yPoints, xPoints) - n * yAverage * xAverage;
+  const crossProductXX = crossProduct(xPoints, xPoints) - n * xAverage * xAverage;
 
   // Calculate Regression Coefficients
-  const slope = cross_product_xy / cross_product_xx;
-  const offset = y_average - slope * x_average;
+  const slope = crossProductXY / crossProductXX;
+  const offset = yAverage - slope * xAverage;
 
   return [slope, offset];
 }
 
-function crossProduct (array_0, array_1) {
+function crossProduct (xArray, yArray) {
   let result = 0;
 
-  for (let i = 0; i < array_0.length; i++) {
-    result += array_0[i] * array_1[i];
+  for (let i = 0; i < xArray.length; i++) {
+    result += xArray[i] * yArray[i];
   }
 
   return result;
