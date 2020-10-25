e9c20a37984ac4b4183e4f2f99be9a63051bdff6
Gourav Rawat <40224985+LON3WO7F@users.noreply.github.com>
2020-10-25 22:07:35 +0530

    Created New File Maximum sum array

---
+++ algorithms/ar-lscsa/python3/max_sum_array.py
@@ -0,0 +1,10 @@
+def maxSubArray(nums):
+       num_array = [0 for i in range(len(nums))]
+       num_array[0] = nums[0]
+       for i in range(1,len(nums)):
+              num_array[i] = max(num_array[i-1]+nums[i],nums[i])
+       return max(num_array)
+
+
+nums = [-13, -3, -25, -20,2,1,-3,7,-2,2,1,-5, -3, -16] 
+print(maxSubArray(nums))
