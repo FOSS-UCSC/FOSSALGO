5b05a36825c570d334790446dd9e0deb01c73e88
Phong Pham <phamphongbsn@outlook.com>
2020-10-26 21:42:06 +0700

    Make inner class to avoid global duplicate class

--- algorithms/gr-dfsrh/java/DepthFirstSearch.java
+++ algorithms/gr-dfsrh/java/DepthFirstSearch.java
@@ -30,87 +30,86 @@ public class DepthFirstSearch {
 		
 		graph.dfs(nodeA);
 	}
+	
+	static class Graph {
 
-}
+		private Map<Node, Set<Node>> adjacencyMap;
 
-class Graph {
-
-	private Map<Node, Set<Node>> adjacencyMap;
-
-	public Graph() {
-		this.adjacencyMap = new HashMap<>();
-	}
-
-	/**
-	 * Add a node into the graph. Optionally add its adjacency node if needed.
-	 * @param node given node will override if same node existed in the graph.
-	 * @param adjacencies
-	 */
-	public void addNode(Node node, Node... adjacencies) {
-		Set<Node> adjacencyNodes = new HashSet<>();
-		for (Node adjacency : adjacencies) {
-			adjacencyNodes.add(adjacency);
-		}
-		adjacencyMap.put(node, adjacencyNodes);
-	}
-
-	/**
-	 * <strong>Depth First Search (DFS)</strong> algorithm traverses a graph in a
-	 * depthward motion and uses a stack to remember to get the next vertex to start
-	 * a search, when a dead end occurs in any iteration.
-	 * 
-	 * @param rootNode the node that algorithm start to traverse
-	 */
-	public void dfs(Node rootNode) {
-		// If start node not existed in the graph, then return
-		if (adjacencyMap.get(rootNode) == null) {
-			return;
+		public Graph() {
+			this.adjacencyMap = new HashMap<>();
 		}
 
-		Deque<Node> stack = new LinkedList<>();
+		/**
+		 * Add a node into the graph. Optionally add its adjacency node if needed.
+		 * @param node given node will override if same node existed in the graph.
+		 * @param adjacencies
+		 */
+		public void addNode(Node node, Node... adjacencies) {
+			Set<Node> adjacencyNodes = new HashSet<>();
+			for (Node adjacency : adjacencies) {
+				adjacencyNodes.add(adjacency);
+			}
+			adjacencyMap.put(node, adjacencyNodes);
+		}
 
-		// Put start node to the stack
-		stack.push(rootNode);
+		/**
+		 * <strong>Depth First Search (DFS)</strong> algorithm traverses a graph in a
+		 * depthward motion and uses a stack to remember to get the next vertex to start
+		 * a search, when a dead end occurs in any iteration.
+		 * 
+		 * @param rootNode the node that algorithm start to traverse
+		 */
+		public void dfs(Node rootNode) {
+			// If start node not existed in the graph, then return
+			if (adjacencyMap.get(rootNode) == null) {
+				return;
+			}
 
-		while (!stack.isEmpty()) {
-			final Node node = stack.pop();
-			if (!node.isVisited()) {
-				// If the node not visit yet, visit it
-				node.visit();
+			Deque<Node> stack = new LinkedList<>();
 
-				// Push adjacency nodes of current visiting node to stack
-				Set<Node> adjacencyNodes = adjacencyMap.get(node);
-				if (adjacencyNodes != null && !adjacencyNodes.isEmpty()) {
-					adjacencyNodes.stream().filter(item -> !item.isVisited())
-							.forEach(item -> stack.push(item));
+			// Put start node to the stack
+			stack.push(rootNode);
+
+			while (!stack.isEmpty()) {
+				final Node node = stack.pop();
+				if (!node.isVisited()) {
+					// If the node not visit yet, visit it
+					node.visit();
+
+					// Push adjacency nodes of current visiting node to stack
+					Set<Node> adjacencyNodes = adjacencyMap.get(node);
+					if (adjacencyNodes != null && !adjacencyNodes.isEmpty()) {
+						adjacencyNodes.stream().filter(item -> !item.isVisited())
+								.forEach(item -> stack.push(item));
+					}
 				}
 			}
 		}
 	}
+	
+	static class Node {
 
-}
+		private String label;
+		private boolean visited;
 
-class Node {
+		public Node(String label) {
+			this.label = label;
+			this.visited = false;
+		}
 
-	private String label;
-	private boolean visited;
+		public String label() {
+			return label;
+		}
 
-	public Node(String label) {
-		this.label = label;
-		this.visited = false;
-	}
+		public boolean isVisited() {
+			return visited;
+		}
 
-	public String label() {
-		return label;
-	}
+		public void visit() {
+			visited = true;
+			System.out.println("I visited node " + this.label);
+		}
 
-	public boolean isVisited() {
-		return visited;
-	}
-
-	public void visit() {
-		visited = true;
-		System.out.println("I visited node " + this.label);
 	}
 
 }
