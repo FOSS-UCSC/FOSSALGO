4af7ba4976f3965d6ba050d1e7b83cae6f718b16
Gourav Rawat <40224985+LON3WO7F@users.noreply.github.com>
2020-10-26 09:09:49 +0530

    Resolved Trailing WhiteSpaces

--- algorithms/ar-lscsa/python3/max_sum_array.py
+++ algorithms/ar-lscsa/python3/max_sum_array.py
@@ -4,5 +4,5 @@ def maxSubArray(nums):
        for i in range(1,len(nums)):
               num_array[i] = max(num_array[i-1]+nums[i],nums[i])
        return max(num_array)
-nums = [-13, -3, -25, -20,2,1,-3,7,-2,2,1,-5, -3, -16] 
+nums = [-13,-3,-25,-20,2,1,-3,7,-2,2,1,-5,-3,-16] 
 print(maxSubArray(nums))
