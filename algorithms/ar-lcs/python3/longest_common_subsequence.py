95fabe07fcc36a58877af54b8f767d432f5d2885
Pavan <56096830+gpk2000@users.noreply.github.com>
2020-10-28 16:07:35 +0000

    added lcs in python3 (#803)
    * added lcs in python3
    
    * Rename LongestCommonSubsequence.py to longest_common_subsequence.py
    
    * Update longest_common_subsequence.py

---
+++ algorithms/ar-lcs/python3/longest_common_subsequence.py
@@ -0,0 +1,20 @@
+def LongestCommonSubsequence(A, B):
+    row, col = len(A), len(B)
+    # constructing the dp matrix intialized with zeros
+    dp = []
+    for _ in range(row + 1):
+        dp.append([0] * (col + 1))
+
+    for i in range(1, row + 1):
+        for j in range(1, col + 1):
+            if A[i - 1] == B[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[row][col]
+
+# testing section
+if __name__ == "__main__":
+    A = "AGGTAB"
+    B = "GXTXAYB"
+    print("Length of LCS is", LongestCommonSubsequence(A, B))
