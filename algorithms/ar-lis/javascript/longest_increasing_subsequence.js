5e5e1fa184c1e122d6432ff8d7db3eedff34997a
Brian Evans <53117772+mrbrianevans@users.noreply.github.com>
2020-10-30 17:57:33 +0000

    Added longest increasing subsequence in JavaScript (Issue #817 ) (#830)
    * Added longest increasing subsequence in Python3 (Issue #820)
    
    * Added longest increasing subsequence in JavaScript (Issue #817)
    
    * Revert "Added longest increasing subsequence in JavaScript (Issue #817)"
    
    This reverts commit 1bb1ea3c
    
    Sorry, trying to keep one issue to one PR policy
    Accidently fixed two issues before the first PR was approved
    
    * Added longest increasing subsequence in JavaScript (Issue #817)
    
    * Added longest increasing subsequence in Python 3
    
    Updated the readme to reflect this addition
    
    * Added longest increasing subsequence in JavaScript to README.md
    
    * Updated longest increasing subsequence in JavaScript
    
    * Updated longest increasing subsequence in JavaScript for Codacy
    
    * Updated longest increasing subsequence in JavaScript for Codacy
    
    Co-authored-by: Danushka Herath <Danushkaherath96@gmail.com>

---
+++ algorithms/ar-lis/javascript/longest_increasing_subsequence.js
@@ -0,0 +1,34 @@
+/**
+ * Longest increasing subsequence is the subsequence that has the longest streak of going up consecutively
+ * @param {number[]} arr the sequence to search for longest subsequence
+ */
+function findLongestIncreasingSubsequence(arr) {
+    let longestSubsequence = [];
+    for (let arrKey in arr) {
+        if (arr.hasOwnProperty(arrKey)) {
+            if (Number(arrKey) + 1 === arr.length) {
+                break;
+            } // stop before the last element
+            let tempLength = 0; // holds the length of the subsequence which is increasing
+            do { // if next element is greater than current element, subsequence is increasing
+                tempLength++;
+            } while (arr[Number(arrKey) + tempLength - 1] < arr[tempLength + Number(arrKey)]);
+            // when subsequence is no longer increasing, check if this subsequence is longer than any other previous one
+            if (tempLength > longestSubsequence.length) {
+                longestSubsequence = arr.slice(Number(arrKey), tempLength + Number(arrKey));
+            }
+        }
+    }
+    return longestSubsequence;
+}
+
+function main() {
+    let input = [
+        3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6,
+        4, 3, 3, 8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6, 9, 3, 9, 9
+    ];
+    let longestIncreasingSubsequence = findLongestIncreasingSubsequence(input);
+    // console.log(longestIncreasingSubsequence)
+}
+
+main();
