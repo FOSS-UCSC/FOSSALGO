172107b69401c355aa6ac98249da967225e387a9
ashutosh007865 <53836584+ashutosh007865@users.noreply.github.com>
2020-10-31 20:42:34 +0530

    Update lcs.c

--- algorithms/ar-lcs/c/lcs.c
+++ algorithms/ar-lcs/c/lcs.c
@@ -1,39 +1,54 @@
-#include<stdio.h>
-#include<string.h>
-int max(int a, int b); 
-int lcs( char *X, char *Y, int m, int n ) 
-{ 
-   int L[m+1][n+1]; 
-   int i, j; 
-   for (i=0; i<=m; i++) 
-   { 
-     for (j=0; j<=n; j++) 
-     { 
-       if (i == 0 || j == 0) 
-         L[i][j] = 0; 
-   
-       else if (X[i-1] == Y[j-1]) 
-         L[i][j] = L[i-1][j-1] + 1; 
-   
-       else
-         L[i][j] = max(L[i-1][j], L[i][j-1]); 
-     } 
-   }
-   return L[m][n]; 
-} 
-int max(int a, int b) 
-{ 
-    return (a > b)? a : b; 
-} 
-int main() 
-{ 
-  char X[] = "AGGTAB"; 
-  char Y[] = "GXTXAYB"; 
-   
-  int m = strlen(X); 
-  int n = strlen(Y); 
-   
-  printf("Length of LCS is %d", lcs( X, Y, m, n ) ); 
-  
-  return 0; 
-} 
+#include <stdio.h>
+#include <string.h>
+
+int i, j, m, n, LCS_table[20][20];
+char S1[20] = "ACADB", S2[20] = "CBDA", b[20][20];
+
+void lcsAlgo() {
+  m = strlen(S1);
+  n = strlen(S2);
+  for (i = 0; i <= m; i++)
+    LCS_table[i][0] = 0;
+  for (i = 0; i <= n; i++)
+    LCS_table[0][i] = 0;
+
+ 
+  for (i = 1; i <= m; i++)
+    for (j = 1; j <= n; j++) {
+      if (S1[i - 1] == S2[j - 1]) {
+        LCS_table[i][j] = LCS_table[i - 1][j - 1] + 1;
+      } else if (LCS_table[i - 1][j] >= LCS_table[i][j - 1]) {
+        LCS_table[i][j] = LCS_table[i - 1][j];
+      } else {
+        LCS_table[i][j] = LCS_table[i][j - 1];
+      }
+    }
+
+  int index = LCS_table[m][n];
+  char lcsAlgo[index + 1];
+  lcsAlgo[index] = '\0';
+
+  int i = m, j = n;
+  while (i > 0 && j > 0) {
+    if (S1[i - 1] == S2[j - 1]) {
+      lcsAlgo[index - 1] = S1[i - 1];
+      i--;
+      j--;
+      index--;
+    }
+
+    else if (LCS_table[i - 1][j] > LCS_table[i][j - 1])
+      i--;
+    else
+      j--;
+  }
+
+  // Printing the sub sequences
+  printf("S1 : %s \nS2 : %s \n", S1, S2);
+  printf("LCS: %s", lcsAlgo);
+}
+
+int main() {
+  lcsAlgo();
+  printf("\n");
+}
