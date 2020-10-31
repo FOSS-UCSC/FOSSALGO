ba2016ac1ffb112b22d5aa7e14101ba765f7a69c
noobcoder1211 <73612688+noobcoder1211@users.noreply.github.com>
2020-10-31 09:10:28 +0530

    Update Longest Increasing Subsequence.cpp

--- algorithms/ar-lis/cpp/Longest Increasing Subsequence.cpp
+++ algorithms/ar-lis/cpp/Longest Increasing Subsequence.cpp
@@ -1,18 +1,37 @@
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        if(nums.size()==0)return 0;
-        vector<int > v;
-        v.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]>v.back()){
-                v.push_back(nums[i]);
-            }
-            else{
-                int idx=lower_bound(v.begin(),v.end(),nums[i])-v.begin();
-                v[idx]=nums[i];
-            }
-        }
-        return v.size();
-    }
-};
+#include<bits/stdc++.h> 
+using namespace std; 
+
+int LongestIncreasingSubsequenceLength(std::vector<int>& v) 
+{ 
+	if (v.size() == 0) 
+		return 0; 
+
+	std::vector<int> tail(v.size(), 0); 
+	int length = 1; // always points empty slot in tail 
+
+	tail[0] = v[0]; 
+	
+	for (int i = 1; i < v.size(); i++) { 
+
+			// Do binary search for the element in 
+			// the range from begin to begin + length 
+		auto b = tail.begin(), e = tail.begin() + length; 
+		auto it = lower_bound(b, e, v[i]); 
+			
+		// If not present change the tail element to v[i] 
+		if (it == tail.begin() + length) 
+		tail[length++] = v[i]; 
+		else
+		*it = v[i]; 
+	} 
+
+	return length; 
+} 
+
+int main() 
+{ 
+	std::vector<int> v{ 2, 5, 3, 7, 11, 8, 10, 13, 6 }; 
+	std::cout << "Length of Longest Increasing Subsequence is "
+			<< LongestIncreasingSubsequenceLength(v); 
+	return 0; 
+} 
