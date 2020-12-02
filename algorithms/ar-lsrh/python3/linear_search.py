1ecf19a10e9cd3a8a9126cdb6cfa1d34d9588c6e
GetsugaTenzo <63441624+GetsugaTenzo@users.noreply.github.com>
2020-12-02 20:00:46 +0530

    Update linear_search.py

--- algorithms/ar-lsrh/python3/linear_search.py
+++ algorithms/ar-lsrh/python3/linear_search.py
@@ -1,5 +1,4 @@
-def linear_search(a,k):
-    
+def linear_search(a,k):  
     l = len(a)
     for i in range( l ):
         print(i,'->',a[i])
@@ -8,28 +7,19 @@ def linear_search(a,k):
     return -1
 
 
-def main():
-        
+def main():        
     choice = input("Are you entering integers ? Enter (yes / no)")
-    
     if choice.lower() == "yes" :
         arr = list(map(int, input("Enter the comma-seperated elements: ").strip().split(',')))
-        key = int(input("Enter an element to search: "))
-        
+        key = int(input("Enter an element to search: "))       
     elif choice.lower() == "no":
         arr = list((input("Enter the comma-seperated elements: ").strip().split(',')))
         key = input("Enter an element to search: ")
-
     index = linear_search(arr,key)
-
-
     if index == -1:
         print("Element {} is not in the list.".format(key))
     else:
         print("Element {} is found at index {}".format(key,index))
-
-
-
+        
 if __name__=="__main__":
-
     main()
