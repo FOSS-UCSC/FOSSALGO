f2b4ffe88503058d1f5c8a7217c89f01db6a70b8
Ashera Silva <asherasilva100@gmail.com>
2021-10-20 17:10:48 +0530

    Add Sleep Sort in Python3

---
+++ algorithms/ar-slpsrt/python3/sleep_sort.py
@@ -0,0 +1,18 @@
+from time import sleep
+from threading import Timer
+
+def sleepSort(values):
+    sortedList = []
+    def add(x):
+        sortedList.append(x)
+    max = values[0]
+    for value in values:
+        if max < value: max = value
+        Timer(value, add, [value]).start()
+    sleep(max+1)
+    return sortedList
+
+list = [3,2,4,5,1]
+print("Original list:", list)
+sortedList = sleepSort(list)
+print("Sorted list:", sortedList)
