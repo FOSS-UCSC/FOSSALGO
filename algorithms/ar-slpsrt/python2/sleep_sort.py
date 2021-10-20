9b71c188387f523aa71874290137e658968582a7
Ashera Silva <asherasilva100@gmail.com>
2021-10-20 17:25:28 +0530

    Update Sleep Sort in Python2

--- algorithms/ar-slpsrt/python2/sleep_sort.py
+++ algorithms/ar-slpsrt/python2/sleep_sort.py
@@ -5,14 +5,14 @@ def sleepSort(values):
     sortedList = []
     def add(x):
         sortedList.append(x)
-    max = values[0]
+    maxVal = values[0]
     for value in values:
-        if max < value: max = value
+        if maxVal < value: maxVal = value
         Timer(value, add, [value]).start()
-    sleep(max+1)
+    sleep(maxVal+1)
     return sortedList
 
-list = [3,2,4,5,1]
-print "Original list:", list
-sortedList = sleepSort(list)
+lst = [3,2,4,5,1]
+print "Original list:", lst
+sortedList = sleepSort(lst)
 print "Sorted list:", sortedList
