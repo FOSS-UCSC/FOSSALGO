ac4f824e67e51daecf6558a2061be1d345d88b9d
Rajendra Kumar R Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2021-10-20 14:06:53 +0530

    Removed all comments and Logging information.
    there is few link in print message which can not be removed!

--- data-structures/ds-stk/csharp/Stack.cs
+++ data-structures/ds-stk/csharp/Stack.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Diagnostics;
+using stack;
 
 namespace stack
 {
@@ -7,7 +8,6 @@ namespace stack
         internal int data;
         internal Node next;
 
-        // Constructor to create a new node.Next is by default initialized as null 
         public Node(int d)
         {
             data = d;
@@ -19,23 +19,11 @@ namespace stack
     {
         private Node _top;
 
-        /// <summary>
-        ///     On creation of Object of Class LinkedList this constructor will be called.
-        ///     It will setup
-        ///     <code>_top = null</code>
-        /// </summary>
         public LinkedListStack()
         {
             _top = null;
         }
 
-        /// <summary>
-        ///     Check, If
-        ///     <code>_top == null</code>
-        ///     means if stack is empty,
-        ///     It returns true else false.
-        /// </summary>
-        /// <returns>bool</returns>
         internal bool isEmpty()
         {
             if (_top == null)
@@ -45,89 +33,57 @@ namespace stack
             return false;
         }
 
-        /// <summary>
-        ///     Push method is used to add element in stack.
-        ///     Stack follow basic rules, Last In First Out (LIFO).
-        ///     means element added at the end will be appended at _top of
-        ///     stack.
-        ///     It will not return any value
-        /// </summary>
-        /// <param name="value"></param>
         internal void Push(int value)
         {
             var newNode = new Node(value);
-            // if stack is null
             if (isEmpty())
             {
                 newNode.next = null;
             }
-            // if already some data exists.
             else
             {
                 newNode.next = _top;
             }
             _top = newNode;
-            // Console.WriteLine("{0} pushed to stack", value);
         }
-
-        /// <summary>
-        ///     It will remove element and return element at _top of stack or
-        ///     remove the element added most recently in stack.
-        /// </summary>
         internal int Pop()
         {
-            // empty stack operation handler.
             if (isEmpty())
             {
-                // Console.WriteLine("Stack Underflow. Nothing is to delete.");
                 return -1;
             }
-
             var currentTop = _top.data;
-            // replacing element at _top with element at _top.next.
             _top = _top.next;
             return currentTop;
         }
-
-        /// <summary>
-        ///     It works as Pop but only return the elements, It is not delete the element
-        ///     from stack.
-        ///     Only it will returns and you can display it on console.
-        /// </summary>
         internal int Peek()
         {
             if (isEmpty())
             {
-                // Console.WriteLine("Stack Underflow.");
                 return -1;
             }
 
             return _top.data;
         }
-
-        /// <summary>
-        ///     As name suggests, It print all the elements
-        /// </summary>
+        
         internal void Print()
         {
             if (isEmpty())
             {
-                // Console.WriteLine("Stack is underflow, Nothing to display yet!");
                 return;
             }
 
             var tmpNode = _top;
             while (tmpNode != null)
             {
-                Console.Write("{0}", tmpNode.data);
+                Debug.Write(tmpNode.data);
                 if (tmpNode.next != null)
                 {
-                    Console.Write("->");
+                    Debug.Write("->");
                 }
                 tmpNode = tmpNode.next;
             }
-
-            Console.WriteLine();
+            Debug.WriteLine("");
         }
 
         internal bool Search(int element)
@@ -141,22 +97,21 @@ namespace stack
                 }
                 return false;
             }
+
             return false;
         }
     }
 }
 
-class Stack
+internal class Stack
 {
-    /**
-        20->30
-        120->50->20->30
-        20->30
-        Element 20 found? : True
-    */
+    protected Stack()
+    {
+    }
+
     public static void Main(string[] args)
     {
-        var llStack = new stack.LinkedListStack();
+        var llStack = new LinkedListStack();
         llStack.Print();
         llStack.Pop();
         llStack.Push(30);
@@ -165,9 +120,9 @@ class Stack
         llStack.Push(50);
         llStack.Push(120);
         llStack.Print();
-        llStack.Pop();  // It is also return the value being removed as int.
+        llStack.Pop();
         llStack.Pop();
         llStack.Print();
-        Console.WriteLine("Element {0} found? : {1}", 20, llStack.Search(20));
+        llStack.Search(20);
     }
 }
