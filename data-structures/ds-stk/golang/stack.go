d74f0b06f2108f60b012e34b8918c8e344508d15
cc1 <chindupriya_c@intuit.com>
2021-10-21 10:33:23 +0530

    fmt go file

--- data-structures/ds-stk/golang/stack.go
+++ data-structures/ds-stk/golang/stack.go
@@ -7,11 +7,11 @@ import (
 //---Stack supports any type of elements-----
 
 type stack struct {
-	items [] interface{}
+	items []interface{}
 }
 
 /*
-Stack is a linear data structure. 
+Stack is a linear data structure.
 Follows Last in First Out (LIFO) principle.
 1. init
 2. Push
@@ -23,24 +23,24 @@ Follows Last in First Out (LIFO) principle.
 */
 
 //---Initialize Stack-----
-func initialize() stack{
-	return stack{};
+func initialize() stack {
+	return stack{}
 }
 
 //---Pushing any type of element to Stack-----
-func (s *stack) push(i interface{}){
+func (s *stack) push(i interface{}) {
 	s.items = append(s.items, i)
 }
 
 //---Pop an element from Stack-----
-func (s *stack) pop() interface{}{
-	if(!s.isEmpty()){
-		stackSize := len(s.items)-1
+func (s *stack) pop() interface{} {
+	if !s.isEmpty() {
+		stackSize := len(s.items) - 1
 		poppedItem := s.items[stackSize]
 		s.items = s.items[:stackSize]
 		return poppedItem
-	} 	
-	fmt.Println("Stack is empty");
+	}
+	fmt.Println("Stack is empty")
 	return ""
 }
 
@@ -51,11 +51,11 @@ func (s *stack) size() int {
 
 //---Peek an element in the Stack-----
 func (s *stack) peek() interface{} {
-	if(!s.isEmpty()){
+	if !s.isEmpty() {
 		return s.items[len(s.items)-1]
-		
-	} 
-	fmt.Println("Stack is empty");
+
+	}
+	fmt.Println("Stack is empty")
 	return ""
 }
 
@@ -66,35 +66,35 @@ func (s *stack) isEmpty() bool {
 
 func main() {
 	fmt.Println("Hello All, Stack Operation in GoLang")
-	
+
 	//Stack Initialize
-	myStack := initialize();
-	
-	//Check stack is Empty before pushing element? 
+	myStack := initialize()
+
+	//Check stack is Empty before pushing element?
 	fmt.Println("IsEmpty", myStack.isEmpty())
-	
+
 	//Pushing any type of elements
-	myStack.push(100);
-	myStack.push(200);
-	myStack.push("first");
-	myStack.push(false);
-	myStack.push(500);
-	
+	myStack.push(100)
+	myStack.push(200)
+	myStack.push("first")
+	myStack.push(false)
+	myStack.push(500)
+
 	//Print Stack: After loaded with elements
 	fmt.Println("Stack data: ", myStack)
-	
+
 	//printing size of stack
 	fmt.Println("Size of the stack", myStack.size())
-	
+
 	//Peek an element
 	fmt.Println("Peek element: ", myStack.peek())
-	
+
 	//Pop an element
 	fmt.Println("Pop element: ", myStack.pop())
-	
+
 	//Check stack is Empty?
 	fmt.Println("IsEmpty", myStack.isEmpty())
-	
+
 	//Print Stack
 	fmt.Println("Stack data: ", myStack)
 }
