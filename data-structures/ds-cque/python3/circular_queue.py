606368fa95e1689272f75a8d3b43adcbcffe7120
AKSHATHA K POOJARY <55616253+AkshathaKPoojary@users.noreply.github.com>
2021-10-22 20:33:35 +0530

    Added circular queue in python

--- data-structures/ds-cque/python3/circular_queue.py
+++ data-structures/ds-cque/python3/circular_queue.py
@@ -1,42 +1,31 @@
 class CircularQueue:
-  
-  def __init__(self, maxSize):
-    self.queue = list()
-    self.maxSize = maxSize
-    self.head = 0
-    self.tail = 0
-
- 
-  def enqueue(self, data):
-    if self.size() == (self.maxSize - 1):
-      return("Queue is full!")
-    else:
-     
-      self.queue.append(data)
-     
-      self.tail = (self.tail+1) % self.maxSize
-      return True
+    def __init__(self, maxSize):
+        self.queue = list()
+        self.maxSize = maxSize
+        self.head = 0
+        self.tail = 0
+    def enqueue(self, data):
+        if self.size() == (self.maxSize - 1):
+            return("Queue is full!")
+        else:
+            self.queue.append(data)
+            self.tail = (self.tail+1) % self.maxSize
+            return True
   
  
-  def dequeue(self):
-   
-    if self.size() == 0:
-      return("Queue is empty!")
-    else:
-      
-      data = self.queue[self.head]
-    
-      self.head = (self.head+1) % self.maxSize
-      return data
-  
- 
-  def size(self):
-    if self.tail >= self.head:
-      qSize = self.tail - self.head
-    else:
-      qSize = self.maxSize - (self.head - self.tail)
-  
-    return qSize
+    def dequeue(self):
+        if self.size() == 0:
+            return("Queue is empty!")
+        else:
+            data = self.queue[self.head]
+            self.head = (self.head+1) % self.maxSize
+            return data
+    def size(self):
+        if self.tail >= self.head:
+            qSize = self.tail - self.head
+        else:
+            qSize = self.maxSize - (self.head - self.tail)
+            return qSize
 
 size = input("Enter the size of the Circular Queue")
 q = CircularQueue(int(size))
