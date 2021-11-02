bd70cdc63660bcf7226d617dfbdf5cc2795cd2d3
Sachintha <ravindusachintha53@gmail.com>
2021-11-02 22:27:55 +0530

    Update queue.py

--- data-structures/ds-que/python2/queue.py
+++ data-structures/ds-que/python2/queue.py
@@ -1,14 +1,16 @@
 
 # Queue Implementation in Python
 
-class Queue: 
+
+class Queue:
     # Queue Initialization
     def __init__(self, size):
-        self.q = [None] * size     
-        self.capacity = size        
-        self.front = 0              
-        self.rear = -1           
+        self.q = [None] * size
+        self.capacity = size
+        self.front = 0
+        self.rear = -1
         self.count = 0
+        
     # Dequeue the Front Element
     def pop(self):
         if self.isEmpty():
@@ -17,6 +19,7 @@ class Queue:
         print("Dequeuing Element", self.q[self.front])
         self.front = (self.front + 1) % self.capacity
         self.count = self.count - 1
+        
     # Add Element to the Queue
     def append(self, value):
         if self.isFull():
@@ -26,21 +29,26 @@ class Queue:
         self.rear = (self.rear + 1) % self.capacity
         self.q[self.rear] = value
         self.count = self.count + 1
+        
     # Return the Front Element of the Queue
     def peek(self):
         if self.isEmpty():
             print("UnderFlow")
             exit(1)
         return self.q[self.front]
+    
     # Return the Size of the Queue
     def size(self):
         return self.count
+    
     # Check if the Queue Is Empty or Not
     def isEmpty(self):
         return self.size() == 0
+    
     # Check if the Queue Is Full or Not
     def isFull(self):
         return self.size() == self.capacity
+    
 if __name__ == '__main__':
     # Queue Creation
     q = Queue(5)
